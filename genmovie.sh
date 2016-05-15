b4b3c864d2d3402c6711cc8ede4a6495869cd521
David Kleiven <davidkleiven446@gmail.com>
2016-05-15 15:07:07 +0100

    Added line source and script for generating movie from hdf5 files

---
+++ genmovie.sh
@@ -0,0 +1,14 @@
+DDIR="data"
+PREFIX="ez"
+PREFIX="${DDIR}/${PREFIX}"
+
+# Convert all hdf5 files to png
+echo "Converting hdf5 files to png..."
+for file in ${PREFIX}*.h5;
+do
+  h5topng ${file} -Zc dkbluered
+done;
+
+# Convert png files to movie
+echo "Converting pngs to animated gif..."
+convert ${PREFIX}*.png ${PREFIX}.gif 
--- src/planeReflection.cpp
+++ src/planeReflection.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 #include "meep.hpp"
 #include <cstring>
+#include <cmath>
+#include <complex>
 
 using namespace std;
 
 const double EPS_LOW = 1.0;
 const double EPS_HIGH = 1.5;
 char OUTDIR[5] = "data";
+const double XSIZE = 5.0;
+const double YSIZE = 10.0;
+const double SOURCE_Y = 9.0;
+const double ANGLE=10.0;
+const double PI = acos(-1.0);
+const complex<double> IMAG_UNIT(0,1.0);
 
 
 double dielectric(const meep::vec &pos)
 { 
-  if ( pos.y() > 5.0 )
+  if ( pos.y() < 5.0 )
   {
     return EPS_HIGH;
   }
   return EPS_LOW;
 }
+
+complex<double> amplitude(const meep::vec &pos)
+{
+  //return 1.0;
+  double lambda = 1.0/0.3;
+  double kx = 2.0*PI/lambda;
+  return exp(IMAG_UNIT*kx*pos.x());
+}
+
+double sourceX()
+{
+  double xcenter = XSIZE/2.0;
+  double delta = SOURCE_Y*tan(ANGLE*PI/180.0);
+  if ( delta > xcenter )
+  {
+    cout << "The angle is too large. Increase size in xdirection\n";
+    return xcenter;
+  }
+  return xcenter-delta;
+} 
 
 int main(int argc, char **argv)
 {
@@ -24,9 +52,11 @@ int main(int argc, char **argv)
   meep::initialize mpi(argc, argv);
 
   double resolution = 20.0; // pixels per distance
-  double xsize = 5.0;
-  double ysize = 10.0;
-  meep::grid_volume vol = meep::vol2d(xsize, ysize, resolution);
+  meep::grid_volume vol = meep::vol2d(XSIZE, YSIZE, resolution);
+  meep::vec srcCorner1(0.0, 9.0);
+  meep::vec srcCorner2(XSIZE, 9.0);
+  meep::volume srcvol(srcCorner1, srcCorner2);
+
   meep::structure srct(vol, dielectric, meep::pml(1.0));
   meep::fields field(&srct);
 
@@ -53,10 +83,11 @@ int main(int argc, char **argv)
   double freq = 0.3;
   double freqwidth = 0.1;
   meep::gaussian_src_time src(freq, freqwidth);
-  meep::vec srcpos(1.0, 8.0);
-  field.add_point_source(meep::Ez, src, srcpos);
+  //meep::vec srcpos(sourceX(), SOURCE_Y);
+  //field.add_point_source(meep::Ez, src, srcpos);
+  field.add_volume_source(meep::Ez, src, srcvol, amplitude);
 
-  unsigned int nOut = 4;
+  unsigned int nOut = 40;
   double dt = field.last_source_time()/static_cast<double>(nOut);
   double nextOutputTime = 0.0;
   while ( field.time() < field.last_source_time() )
