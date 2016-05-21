fa98be044b20d8de9b5adadf314d2c70914188aa
David Kleiven <davidkleiven446@gmail.com>
2016-05-21 11:45:53 +0100

    Removed plot commands which is no longer used

--- genPlaneReflectionPlots.sh
+++ genPlaneReflectionPlots.sh
@@ -14,14 +14,3 @@ OFILE="${FDIR}/normalInc.png"
 IFILE="${DDIR}/NormalInc/ezMonitor.csv"
 gnuplot -e "ofile='${OFILE}';ifile='${IFILE}'" plotFieldComponent.plt
 
-echo "Subtracting off background..."
-# Subtract of the background
-BKGFILE="${DDIR}/NoBoundary/ezMonitor.csv"
-./subtractBkg.out "${DDIR}/NormalInc/ezMonitor.csv" ${BKGFILE} "${DDIR}/NormalInc/ezMonitorDiff.csv"
-
-# Plot DIFF files
-echo "Plotting reflected fields..."
-# Normal incidence
-OFILE="${FDIR}/normalIncDiff.png"
-IFILE="${DDIR}/NormalInc/ezMonitorDiff.csv"
-gnuplot -e "ofile='${OFILE}';ifile='${IFILE}'" plotFieldComponent.plt
