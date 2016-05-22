0bae669d453cda74096af84a02b8941a824ede89
David Kleiven <davidkleiven446@gmail.com>
2016-05-22 12:41:46 +0100

    Fixed if test for checking for directory

--- runPlaneReflection.sh
+++ runPlaneReflection.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 # This script runs the plane reflection program with different parameters
-RUN_ALL=false # If false only non-exsisting simulations will be started
+RUN_ALL=true # If false only non-exsisting simulations will be started
 
-DDIR=( "dataPlane/NoBoundary" "dataPlane/NormalInc" "dataPlane/Inc5" "dataPlane/Inc10")
-EPS_HIGH=(1.0 2.25 2.25 2.25)
-ANGLE=(0.0 0.0 5.0 10.0)
+DDIR=( "dataPlane/NoBoundary" "dataPlane/NormalInc")
+EPS_HIGH=(1.0 2.25)
+ANGLE=(0.0 0.0)
 
 POLARIZATION="s"
 for ((i=0;i<${#DDIR[@]};i++));
 do
-  if [ ${RUN_ALL} ]; then
-    DIR=${DDIR[$i]}
+  DIR=${DDIR[$i]}
+  if [ ${RUN_ALL} = true ]; then
     # Clean directory
     rm -f ${DIR}/*.h5 ${DIR}/*.png ${DIR}/*.csv ${DIR}/*.gif
     
     # Run simulation
     ./planeReflection.out ${DIR} ${EPS_HIGH[$i]} ${ANGLE[$i]} ${POLARIZATION}
 
-  elif [! -d ${DIR}]
+  elif [ ! -d "$DIR" ]; then
     # Directory does not exist
     mkdir ${DIR}
     # Run simulation
