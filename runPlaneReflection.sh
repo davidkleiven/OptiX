d634e33af03add09ca057825df2bf102c65ef559
David Kleiven <davidkleiven446@gmail.com>
2016-05-22 11:57:55 +0100

    Added more angles

--- runPlaneReflection.sh
+++ runPlaneReflection.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # This script runs the plane reflection program with different parameters
-RUN_ALL=true # If false only non-exsisting simulations will be started
+RUN_ALL=false # If false only non-exsisting simulations will be started
 
-DDIR=( "dataPlane/NoBoundary" "dataPlane/NormalInc" )
-EPS_HIGH=(1.0 2.25)
-ANGLE=(0.0 0.0)
+DDIR=( "dataPlane/NoBoundary" "dataPlane/NormalInc" "dataPlane/Inc5" "dataPlane/Inc10")
+EPS_HIGH=(1.0 2.25 2.25 2.25)
+ANGLE=(0.0 0.0 5.0 10.0)
 
+POLARIZATION="s"
 for ((i=0;i<${#DDIR[@]};i++));
 do
   if [ ${RUN_ALL} ]; then
@@ -14,7 +15,13 @@ do
     rm -f ${DIR}/*.h5 ${DIR}/*.png ${DIR}/*.csv ${DIR}/*.gif
     
     # Run simulation
-    ./planeReflection.out ${DIR} ${EPS_HIGH[$i]} ${ANGLE[$i]}
+    ./planeReflection.out ${DIR} ${EPS_HIGH[$i]} ${ANGLE[$i]} ${POLARIZATION}
+
+  elif [! -d ${DIR}]
+    # Directory does not exist
+    mkdir ${DIR}
+    # Run simulation
+    ./planeReflection.out ${DIR} ${EPS_HIGH[$i]} ${ANGLE[$i]} ${POLARIZATION}
   fi
 done
 
