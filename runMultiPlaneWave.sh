1c9c17d76faa11df75a523a83d3c1fbe50cd066a
David Kleiven <davidkleiven446@gmail.com>
2016-05-25 18:57:35 +0100

    Script for running multiple angle scattering

---
+++ runMultiPlaneWave.sh
@@ -0,0 +1,17 @@
+DDIR=dataPlane/MultInc30
+EPS_HIGH=2.25
+ANGLE=30
+
+# Compute background
+BKGDIR=${DDIR}/bkg
+mkdir -p ${BKGDIR}
+rm -f ${BKGDIR}/*
+
+./planeReflection.out ${BKGDIR} 1.0 ${ANGLE} s
+
+# Compute with reflection
+ODIR=${DDIR}/WithEps
+mkdir -p ${ODIR}
+rm -f ${ODIR}/*
+
+./planeReflection.out ${ODIR} ${EPS_HIGH} ${ANGLE} s
