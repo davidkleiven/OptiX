9d2cd7be8c6b5825e1abd7bfccc016289c5de0cd
David Kleiven <davidkleiven446@gmail.com>
2016-05-28 15:12:27 +0100

    Minor change

--- genmovie.sh
+++ genmovie.sh
@@ -7,8 +7,14 @@ echo "Converting hdf5 files to png..."
 for file in ${PREFIX}*.h5;
 do
   h5topng ${file} -Zc dkbluered
+  FILENOEXT=${file%.*}
+  PNGFILE="${FILENOEXT}.png"
+  JPGFILE="${FILENOEXT}.jpg"
+  convert ${PNGFILE} -resize 800x400 ${JPGFILE}
+  rm ${PNGFILE}
 done;
 
 # Convert png files to movie
-echo "Converting pngs to animated gif..."
-convert ${PREFIX}*.png ${PREFIX}.gif 
+echo "Converting jpgs to animated gif..."
+convert ${PREFIX}*.jpg ${PREFIX}.gif 
+rm ${PREFIX}*.jpg
--- runMultiPlaneWave.sh
+++ runMultiPlaneWave.sh
@@ -1,6 +1,6 @@
 DDIR=dataPlane/MultInc30
 EPS_HIGH=2.25
-ANGLE=75
+ANGLE=0
 
 # Compute background
 BKGDIR=${DDIR}/bkg
