ddbfc91dc5798939adfc4a4fec89b96f57e51f81
David Kleiven <davidkleiven446@gmail.com>
2016-06-04 08:14:12 +0100

    Reduced the number of freq in the narrow bandwidth runs

--- runMultiPlaneWave.sh
+++ runMultiPlaneWave.sh
@@ -2,7 +2,7 @@ DDIR_BASE=dataPlane/MultInc
 EPS_HIGH=2.25
 ANGLES=(20 45 75 85)
 REL_BAND_WIDTHS=(0.5 0.5 0.05 0.01)
-NFREQ=(200 200 200 200)
+NFREQ=(200 200 20 20)
 
 # NOTE: It is important that the background run and the actual run is executed succesively.
 # The actual run relies on a temporary file created by the background run.
