b5020e7368e60e1abed6606520469461c700f47d
David Kleiven <davidkleiven446@gmail.com>
2016-06-04 11:26:51 +0100

    Removed files which are no longer used

--- inc/dataToFile.h
+++
@@ -1,8 +0,0 @@
-#ifndef DATA_TO_FILE_H
-#define DATA_TO_FILE_H
-#include <string>
-#include <vector>
-#include "meep.hpp"
-typedef std::vector<double> dvec;
-void monitorToFile(const std::string &fname, const dvec &time, const dvec &fieldVal, const meep::vec &pos);
-#endif
