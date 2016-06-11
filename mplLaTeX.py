0a97b7ccb40392e56211cba56d9655ce8c083667
David Kleiven <davidkleiven446@gmail.com>
2016-06-11 14:32:29 +0100

    LaTeX parameters for matplotlib

---
+++ mplLaTeX.py
@@ -0,0 +1,13 @@
+params = {'backend': 'ps',
+       'text.latex.preamble': ['\usepackage{gensymb}\usepackage{siunitx}'],
+       'axes.labelsize': 8, # fontsize for x and y labels (was 10)
+       'axes.titlesize': 8,
+       'font.size': 8, # was 10
+       'legend.fontsize': 8, # was 10
+       'xtick.labelsize': 8,
+       'ytick.labelsize': 8,
+       'text.usetex': True,
+       'font.family': 'serif',
+       'axes.linewidth':0.1,
+       'figure.figsize': [3.5,2.25]
+}
