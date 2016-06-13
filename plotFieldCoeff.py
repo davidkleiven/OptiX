ec022f81be040fa9334f51492339696293efcc0c
David Kleiven <davidkleiven446@gmail.com>
2016-06-13 19:16:10 +0100

    Plotting both rs and rp. Plotting phase on separate plot

--- plotFieldCoeff.py
+++ plotFieldCoeff.py
@@ -5,8 +5,8 @@ mpl.rcParams.update(mplLaTeX.params)
 from matplotlib import pyplot as plt
 import json
 
-#FOLDERS_s = ["dataPlane/MultInc20/WithEps", "dataPlane/MultInc45/WithEps", "dataPlane/MultInc75/WithEps", "dataPlane/MultInc85/WithEps"]
-FOLDERS_s = ["dataPlane/MultInc20/WithEps"]
+FOLDERS_s = ["dataPlane/MultInc20/WithEps", "dataPlane/MultInc45/WithEps", "dataPlane/MultInc75/WithEps", "dataPlane/MultInc85/WithEps"]
+#FOLDERS_s = ["dataPlane/MultInc20/WithEps"]
 FOLDERS_p = ["dataPlane/MultInc20p/WithEps", "dataPlane/MultInc45p/WithEps", "dataPlane/MultInc75p/WithEps", \
 "dataPlane/MultInc85p/WithEps"]
 
@@ -17,37 +17,66 @@ def rs(theta, n1, n2):
     theta = theta*np.pi/180.0
     thetaT = transmittedTheta(theta, n1, n2)
     return ( n1*np.cos(theta) - n2*np.cos(thetaT) )/( n1*np.cos(theta) + n2*np.cos(thetaT) )
+
+def rp(theta, n1, n2): 
+    theta = theta*np.pi/180.0
+    thetaT = transmittedTheta(theta, n1, n2)
+    return ( n1*np.cos(thetaT) - n2*np.cos(theta) )/( n1*np.cos(thetaT) + n2*np.cos(theta) )
     
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(111)
+    figPhase = plt.figure()
+    axPhase = figPhase.add_subplot(111)
     theta = np.linspace(0.0, 90.0, 101)
     n1 = 1.0
     n2 = 1.5
     ax.plot( theta, np.abs(rs(theta, n1, n2)), color='black' ) 
-    ax2 = ax.twinx()
+    ax.plot( theta, np.abs(rp(theta, n1, n2)), color='black' ) 
 
     msize = 2
-    for folder in FOLDERS_s:
-        fname = folder+'/realFieldFourier.json'
+    step = 5
+    for i in range(0, len(FOLDERS_s)):
+        fname_s = FOLDERS_s[i]+'/realFieldFourier.json'
+        fname_p = FOLDERS_p[i]+'/realFieldFourier.json'
         try:
-            infile = open( fname, 'r')
+            infile = open( fname_s, 'r')
+            data = json.load(infile)
+            infile.close()
         except:
-            print ("Could not open file %s"%(fname))
+            print ("Could not open file %s"%(fname_s))
             continue  
 
-        data = json.load(infile)
-        infile.close()
+        try:
+            infile = open( fname_p, 'r' )
+            data_p = json.load(infile)
+            infile.close()
+        except:
+            print ("Could not open file %s"%(fname_p))
+            continue
 
-        reflectionNorm = np.array( data["reflection"]["norm"] )
-        reflectionPhase = np.array( data["reflection"]["phase"] )
+        reflectionNorm = np.array( data["reflection"]["norm"] )[0:-1:step]
+        reflectionPhase = np.array( data["reflection"]["phase"] )[0:-1:step]
+        angle = np.array( data["angle"] )[0:-1:step]
+
+        refPNorm = np.array( data_p["reflection"]["norm"] )[0:-1:step]
+        refPPhase = np.array( data_p["reflection"]["phase"])[0:-1:step]
+        angle_p = np.array( data_p["angle"] )[0:-1:step]
         
-        ax.plot( data["angle"], reflectionNorm, 'o', color='black', ms=msize)
-        ax2.plot( data["angle"], reflectionPhase, '.', color='grey', ms=msize)
+        ax.plot( angle, reflectionNorm, 'o', color='black', ms=msize)
+        ax.plot( angle_p, refPNorm, '^', color='black', ms=msize)
+        axPhase.plot( angle, reflectionPhase, '.', color='black', ms=msize)
 
-    ax.set_ylim(-1.0, 1.0)
-    ax2.set_ylim(-np.pi/2.0,np.pi/2.0)
+    ax.set_ylim(0.0, 1.0)
+    axPhase.set_ylim(-np.pi,np.pi)
+
+    # Set proper labels on the phase plot
+    ymarks = np.array([-np.pi, -np.pi/2.0, 0.0, np.pi/2.0, np.pi])
+    axPhase.set_yticks(ymarks)
+    ytics = ["$-\pi$", "$\\frac{\pi}{2}$", "0", "$\\frac{\pi}{2}$", "$\pi$"]
+    axPhase.set_yticklabels(ytics)
     fig.savefig( "Figures/fieldCoefficients.pdf", bbox_inches="tight" )
+    figPhase.savefig( "Figures/fieldCoefficientPhase.pdf", bbox_inches="tight" )
 
 if __name__ == '__main__':
     main()
