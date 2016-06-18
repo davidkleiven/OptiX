624f30d2e94bb913ce18843478d925782a9faf06
David Kleiven <davidkleiven446@gmail.com>
2016-06-18 16:58:35 +0100

    Correcting the phase

--- computeReflectionAngle.py
+++ computeReflectionAngle.py
@@ -11,6 +11,7 @@ FOLDERS = ["dataPlane/MultInc5/WithEps", "dataPlane/MultInc20/WithEps", "dataPla
 "dataPlane/MultInc20p/WithEps", "dataPlane/MultInc45p/WithEps", \
 "dataPlane/MultInc75p/WithEps", "dataPlane/MultInc85p/WithEps"]
 
+FOLDERS=["dataPlane/MultInc5/WithEps", "dataPlane/MultInc20/WithEps"]
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -26,10 +27,11 @@ def main():
             print ("Could not find file %s"%(fname))
             continue
         distanceFromPlane = data["geometry"]["sourcePosition"] - data["geometry"]["slabPosition"]
-        k = 0.3
         phase = np.array( data["reflection"]["phase"] )
-        tanAngleNew = np.tan( np.array( data["angle"] )*np.pi/180.0)
-        tanReflTimesTanAngleNew = np.abs( phase*np.sqrt(1.0+tanAngleNew**2)/(k*distanceFromPlane) - tanAngleNew*tanAngleNew )
+        tanAngleNew =  np.tan( np.array( data["angle"] )*np.pi/180.0) 
+        k = 2.0*np.pi*np.array( data["frequency"] )
+        tanReflTimesTanAngleNew = phase*np.sqrt(1.0+tanAngleNew**2)/(k*distanceFromPlane) - tanAngleNew*tanAngleNew 
+
         if ( tanReflTimesTanAngle is None ):
             tanReflTimesTanAngle = np.zeros(len(tanReflTimesTanAngleNew))
             tanAngle = np.zeros(len(tanAngleNew))
@@ -38,12 +40,14 @@ def main():
         else:
             tanReflTimesTanAngle = np.append(tanReflTimesTanAngle, tanReflTimesTanAngleNew)
             tanAngle = np.append(tanAngle, tanAngleNew)
-    slope, interscept, pvalue, rvalye, stderr = stats.linregress(np.log(tanAngle), np.log(tanReflTimesTanAngle))
+    #slope, interscept, pvalue, rvalye, stderr = stats.linregress(np.log(tanAngle), np.log(tanReflTimesTanAngle))
     ax.plot( tanAngle, tanReflTimesTanAngle, '.', color="black", markersize=1, fillstyle="none")
-    ax.plot( tanAngle, np.exp(interscept)*tanAngle**slope, color='black')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    print ("Exponent: %.2f"%(slope))
+    #ax.plot( tanAngle, np.exp(interscept)*tanAngle**slope, color='black')
+    #ax.set_xscale('log')
+    #ax.set_yscale('log')
+    ax.set_xlabel("$\\tan \\theta_i$")
+    ax.set_ylabel("$\\tan \\theta_i \\tan \\theta_r$")
+    #print ("Exponent: %.2f"%(slope))
     fig.savefig("Figures/tanReflection.pdf", bbox_inches="tight")
 
 if __name__ == "__main__":
