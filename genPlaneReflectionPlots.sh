d97c35fd4518e12f5b53dc24ac63eba9d5a5d195
David Kleiven <davidkleiven446@gmail.com>
2016-06-19 13:39:30 +0100

    Using the python scripts for plotting the field coefficients

--- genPlaneReflectionPlots.sh
+++ genPlaneReflectionPlots.sh
@@ -1,35 +1,15 @@
 # This script processes the output from the plane reflection simulation
 DDIR="dataPlane"
-FDIR="Figures/planeReflection"
+FDIR="Figures"
 
-mkdir -p ${FDIR}
-echo "Plotting raw fields at monitor..."
-# Generate plot for the case with no reflections
-OFILE="${FDIR}/noBoundarySource.png"
-IFILE="${DDIR}/NoBoundary/ezMonitorSource.csv"
-gnuplot -e "ofile='${OFILE}';ifile='${IFILE}'" plotFieldComponent.plt
-OFILE="${FDIR}/noBoundaryTrans.png"
-IFILE="${DDIR}/NoBoundary/ezMonitorTrans.csv"
-gnuplot -e "ofile='${OFILE}';ifile='${IFILE}'" plotFieldComponent.plt
+# Plot flux spectrum
+echo "Plotting flux spectrum..."
+python plotFlux.py
 
-# Normal incidence
-OFILE="${FDIR}/normalIncSource.png"
-IFILE="${DDIR}/NormalInc/ezMonitorSource.csv"
-gnuplot -e "ofile='${OFILE}';ifile='${IFILE}'" plotFieldComponent.plt
+# Plot amplitudes
+echo "Plotting amplitudes..."
+python plotFieldCoeff.py
 
-OFILE="${FDIR}/normalIncTrans.png"
-IFILE="${DDIR}/NormalInc/ezMonitorTrans.csv"
-gnuplot -e "ofile='${OFILE}';ifile='${IFILE}'" plotFieldComponent.plt
-
-# Sinc functions
-OFILE="${FDIR}/sincSourceBkg.png"
-IFILE="${DDIR}/MultInc30/bkg/ezMonitorSource.csv"
-gnuplot -e "ofile='${OFILE}';ifile='${IFILE}'" plotFieldComponent.plt
-
-OFILE="${FDIR}/sincSourceWithEps.png"
-IFILE="${DDIR}/MultInc30/WithEps/ezMonitorSource.csv"
-gnuplot -e "ofile='${OFILE}';ifile='${IFILE}'" plotFieldComponent.plt
-
-OFILE="${FDIR}/sincSourceWithEpsTrans.png"
-IFILE="${DDIR}/MultInc30/WithEps/ezMonitorTrans.csv"
-gnuplot -e "ofile='${OFILE}';ifile='${IFILE}'" plotFieldComponent.plt
+# Plot reflection angle
+echo "Plotting reflection angle..."
+python computeReflectionAngle.py
