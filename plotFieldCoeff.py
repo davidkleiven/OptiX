5fc0614a3294eaa141d2d49ee94b9fbdf13bc7bc
David Kleiven <davidkleiven446@gmail.com>
2016-06-20 13:34:18 +0100

    Added error plot

--- plotFieldCoeff.py
+++ plotFieldCoeff.py
@@ -35,8 +35,8 @@ def tp(theta, n1, n2):
 def main():
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    figPhase = plt.figure()
-    axPhase = figPhase.add_subplot(111)
+    figError = plt.figure()
+    axError = figError.add_subplot(111)
     theta = np.linspace(0.0, 90.0, 101)
     n1 = 1.0
     n2 = 1.5
@@ -70,30 +70,46 @@ def main():
         transmissionNorm = np.array( data["transmitted"]["norm"] )[0:-1:step]
         angleR = np.array( data["reflected"]["angle"] )[0:-1:step]
         angleT = np.array( data["transmitted"]["angle"] )[0:-1:step]
+        errorRs = np.abs( reflectionNorm**2 - rs(angleR, n1, n2)**2 )/rs(angleR, n1, n2)**2
+        errorTs = np.abs( transmissionNorm**2 - ts(angleT, n1, n2)**2)/ts(angleT, n1, n2)**2
 
         refPNorm = np.array( data_p["reflected"]["norm"] )[0:-1:step]
         transPNorm = np.array( data_p["transmitted"]["norm"] )[0:-1:step]
         angle_pR = np.array( data_p["reflected"]["angle"] )[0:-1:step]
         angle_pT = np.array( data_p["transmitted"]["angle"])[0:-1:step]
+        errorRp = np.abs( refPNorm**2 - rp(angle_pR, n1, n2)**2 )/rp(angle_pR, n1, n2)**2
+        errorTp = np.abs( transPNorm**2 - tp(angle_pT, n1, n2)**2 )/tp(angle_pT, n1, n2)**2
         
         if ( i==0 ):
             ax.plot( angleR, reflectionNorm**2, 'o', color='black', ms=msize, fillstyle="none", label="$|r_\mathrm{s}|^2$")
             ax.plot( angle_pR, refPNorm**2, '^', color='black', ms=msize, label="$|r_\mathrm{p}|^2$")
             ax.plot( angleT, transmissionNorm**2, 'x', color='black', ms=msize, label="$|t_\matrhrm{s}|^2$")
             ax.plot( angle_pT, transPNorm**2, '.', color='black', ms=msize, label="$|t_\mathrm{p}|^2$")
+            
+            axError.plot( angleR, errorRs, 'o', color='black', ms=msize, fillstyle="none", label="$|r_\mathrm{s}|^2$" )
+            axError.plot( angleT, errorTs, '^', color='black', ms=msize, fillstyle="none", label="$|t_\mathrm{s}|^2$" )
+            axError.plot( angle_pR, errorRp, 'x', color='black', ms=msize, label="$|r_\mathrm{p}|^2$")
+            axError.plot( angle_pT, errorTp, '.', color='black', ms=msize, label="$|t_\mathrm{p}|^2$")
         else:
             ax.plot( angleR, reflectionNorm**2, 'o', color='black', ms=msize, fillstyle="none")
             ax.plot( angle_pR, refPNorm**2, '^', color='black', ms=msize)
             ax.plot( angleT, transmissionNorm**2, 'x', color='black', ms=msize)
             ax.plot( angle_pT, transPNorm**2, '.', color='black', ms=msize)
 
+            axError.plot( angleR, errorRs, 'o', color='black', ms=msize, fillstyle="none")
+            axError.plot( angleT, errorTs, '^', color='black', ms=msize, fillstyle="none")
+            axError.plot( angle_pR, errorRp, 'x', color='black', ms=msize)
+            axError.plot( angle_pT, errorTp, '.', color='black', ms=msize)
+
     ax.set_ylim(0.0, 1.0)
     ax.set_xlabel("Incident angle (deg)")
     ax.set_ylabel("$|r_\mathrm{s}|^2$, $|r_\mathrm{p}|^2$, $|t_\mathrm{s}|^2$, $|t_\mathrm{p}|^2$")
     ax.legend(loc="upper left", ncol=2, frameon=False)
-
-    # Set proper labels on the phase plot
+    axError.set_yscale('log')
+    axError.set_xlabel("Incident angle (deg)")
+    axError.set_ylabel("Relative error")
+    axError.legend(loc='upper left', frameon=False)
     fig.savefig( "Figures/fieldCoefficients.pdf", bbox_inches="tight" )
-
+    figError.savefig("Figures/fieldCoefficientsError.pdf", bbox_inches="tight")
 if __name__ == '__main__':
     main()
