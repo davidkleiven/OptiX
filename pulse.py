2f9cf0fb59a37ecb5fd09735e5b8b168af8390f6
David Kleiven <davidkleiven446@gmail.com>
2016-06-23 15:07:21 +0100

    Writing plot to LaTeX quality. Added descriptive labels

--- pulse.py
+++ pulse.py
@@ -1,8 +1,12 @@
 import numpy as np
+import matplotlib
+import mplLaTeX as mltx
+matplotlib.rcParams.update(mltx.params)
 from matplotlib import pyplot as plt
 import json
 
 def main():
+    ofname = "Figures/pulse20deg.pdf"
     fname = "dataPlane/MultInc20s/WithEps/realField.json"
     fnameBkg = "dataPlane/MultInc20s/bkg/realField.json"
     infile = open( fname, 'r' )
@@ -33,14 +37,19 @@ def main():
 
     # Compute end point
     positionOfMax = np.array( [np.argmax( reflBkgReal ), np.argmax( reflectedWave ), np.argmax( transReal )] )
-    endPoint = 2.0*np.max( positionOfMax )
+    endPoint = 2*np.max( positionOfMax )
     ax.plot( t[:endPoint], reflectedWave[:endPoint], color='black')
     ax.plot( t[:endPoint], reflBkgReal[:endPoint]+shiftBkg, color='black')
     ax.plot( t[:endPoint], transReal[:endPoint]-shiftTrans, color='black')
+
+    x1, x2 = ax.get_xlim()
+    xpos = 0.75*(x2-x1)+x1
+    ax.text( xpos, shiftBkg + 0.2*maxReflected, "Incident" )
+    ax.text( xpos, 0.2*maxReflected, "Reflected")
+    ax.text( xpos, -shiftTrans+0.2*maxReflected, "Transmitted")
     ax.set_xlabel( "Time (s)" )
     ax.set_ylabel( "Amplitude" )
-    fig.show()
-    plt.show()
+    fig.savefig( ofname, bbox_inches="tight")
 
 if __name__ == '__main__':
     main()
