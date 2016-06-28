06662ad1b6c6ab891b83b0b1c1f8aec28045c901
David Kleiven <davidkleiven446@gmail.com>
2016-06-28 08:28:32 +0100

    Power coefficients takes now command line arguments

--- genPlaneReflectionPlots.sh
+++ genPlaneReflectionPlots.sh
@@ -1,10 +1,11 @@
 # This script processes the output from the plane reflection simulation
-DDIR="dataPlane"
+DDIR="dataPlane/MultInc" # Assumes that continues with <angle><polarisation>. Example: MultInc20s
+INC_ANGLE=(5 20 45 75 85)
 FDIR="Figures"
 
 # Plot flux spectrum
 echo "Plotting flux spectrum..."
-python plotFlux.py
+python plotFlux.py ${DDIR} ${FDIR} ${INC_ANGLE[@]}
 
 # Plot amplitudes
 echo "Plotting amplitudes..."
