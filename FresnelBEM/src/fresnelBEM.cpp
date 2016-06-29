5da3998accc6229b38e4d5747ec5945633f3b93e
David Kleiven <davidkleiven446@gmail.com>
2016-06-29 12:08:54 +0100

    Defined source

--- FresnelBEM/src/fresnelBEM.cpp
+++ FresnelBEM/src/fresnelBEM.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <libscuff.h>
+#include <complex>
 
 int main(int argc, char **argv)
 {
@@ -12,7 +13,15 @@ int main(int argc, char **argv)
   HMatrix *matrix = geo.AllocateBEMMatrix();
   HVector *rhsVec = geo.AllocateRHSVector();
 
+  // Source definition
   double sourcePosition[3] = {0.0,0.0,0.3};
+  double kHat[3] = {0.0,0.0,-1.0};
+  std::complex<double> E0[3] = {1.0,0.0,0.0}; 
+  PlaneWave pw(E0, kHat);
+  double omega = 1.0;
+  
+  // Array for storing the fields EH={Ex,Ey,Ez,Hx,Hy,Hz}
+  std::complex<double> EH[6];
 
   delete matrix;
   delete rhsVec;
