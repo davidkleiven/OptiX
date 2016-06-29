05aa4429ea52d9fcd968eb8b1758460cbe8604e3
David Kleiven <davidkleiven446@gmail.com>
2016-06-29 05:39:41 +0100

    Changed folder

--- genPlaneReflectionPlots.sh
+++ genPlaneReflectionPlots.sh
@@ -1,7 +1,7 @@
 # This script processes the output from the plane reflection simulation
-DDIR="dataPlane/MultInc" # Assumes that continues with <angle><polarisation>. Example: MultInc20s
+DDIR="dataPlane/VarResMultInc" # Assumes that continues with <angle><polarisation>. Example: MultInc20s
 INC_ANGLE=(5 20 45 75 85)
-FDIR="Figures"
+FDIR="Figures/VarRes"
 
 # Plot flux spectrum
 echo "Plotting flux spectrum..."
