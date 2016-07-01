c3b67187dc5b50c554d686324779e32232180ad0
David Kleiven <davidkleiven446@gmail.com>
2016-07-01 09:46:35 +0100

    Added functions for computing incident flux and scattered flux

--- FresnelBEM/inc/FluxIntegrator.h
+++ FresnelBEM/inc/FluxIntegrator.h
@@ -10,11 +10,21 @@ class FluxIntegrator
     double compute( const scuff::RWGGeometry &geo, const IncField &IF, double omega, const double kBloch[2] );
     void setZpos(double zpos){_zpos=zpos;};
     void setnEvalPoints( unsigned int nPoints );
+    void setYmin(double ymin){_ymin=ymin;};
+    void setYmax(double ymax){_ymax=ymax;};
+    void setXmin(double xmin){_xmin=xmin;};
+    void setXmax(double xmax){_xmax=xmax;};
+    void fillEvaluationXY();
+
+    double incidentFlux( scuff::RWGGeometry &geo, IncField &IF, double omega, double kBloch[2] );
+    double scatteredFlux( scuff::RWGGeometry &geo, HVector &vec, double omega, double kBloch[2] );
   private:
     double _zpos;
     unsigned int _nEvalPointsInEachDirection;
+    double _ymin, _ymax, _xmin, _xmax;
     HMatrix *_evaluationCrd;
     HMatrix *_flux;
+    double compute( scuff::RWGGeometry &geo, IncField *IF, HVector *vec, double omega, double kBloch[2] );
 };
     
 #endif
--- FresnelBEM/src/FluxIntegrator.cpp
+++ FresnelBEM/src/FluxIntegrator.cpp
@@ -1,6 +1,22 @@
 #include "FluxIntegrator.h"
+#include <complex>
+using namespace std;
 
-FluxIntegrator::FluxIntegrator(): _zpos(0.0), _nEvalPointsInEachDirection(0), _evaluationCrd(NULL), _flux(NULL){};
+FluxIntegrator::FluxIntegrator(): _zpos(0.0), _nEvalPointsInEachDirection(0), _evaluationCrd(NULL), _flux(NULL),\
+_xmin(0.0), _xmax(1.0), _ymin(0.0), _ymax(1.0){};
+
+FluxIntegrator::~FluxIntegrator()
+{
+  if ( _evaluationCrd != NULL )
+  {
+    delete _evaluationCrd;
+  }
+
+  if ( _flux != NULL )
+  {
+    delete _flux;
+  }
+}
 
 void FluxIntegrator::setnEvalPoints(unsigned int nPoints)
 {
@@ -20,4 +36,59 @@ void FluxIntegrator::setnEvalPoints(unsigned int nPoints)
   _flux = new HMatrix ( nPoints*nPoints, 3 );
 }
 
+void FluxIntegrator::fillEvaluationXY()
+{
+  if ( _nEvalPointsInEachDirection == 0 )
+  {
+    this->setnEvalPoints(20);
+  } 
 
+  // Fill evaluation points
+  double dx = (_xmax-_xmin)/static_cast<double>(_nEvalPointsInEachDirection);
+  double dy = (_ymax-_ymin)/static_cast<double>(_nEvalPointsInEachDirection);
+  double x = _xmin;
+  for ( unsigned int ix=0;ix<_nEvalPointsInEachDirection;ix++ )
+  {
+    double y = _ymin;
+    for ( unsigned int iy=0;iy<_nEvalPointsInEachDirection;iy++)
+    {
+      _evaluationCrd->SetEntry( ix*_nEvalPointsInEachDirection+iy, 0, x);
+      _evaluationCrd->SetEntry( ix*_nEvalPointsInEachDirection+iy, 1, y);
+      _evaluationCrd->SetEntry( ix*_nEvalPointsInEachDirection+iy, 2, _zpos);
+      y += dy;
+    }
+    x += dx;
+  }
+}    
+
+double FluxIntegrator::compute( scuff::RWGGeometry &geo, IncField *IF, HVector *vec, double omega, double kBloch[2] )
+{
+  if ( _nEvalPointsInEachDirection == 0 )
+  {
+    this->fillEvaluationXY();
+  }
+
+  // Get field at all evaluation points      
+  geo.GetFields(IF, vec, omega, kBloch, _evaluationCrd, _flux);
+  complex<double> Ex, Ey, Hx, Hy; 
+  double zFlux = 0.0;
+  for ( unsigned int i=0;i<_nEvalPointsInEachDirection;i++ )
+  {
+    Ex = _flux->GetEntry(i,0);
+    Ey = _flux->GetEntry(i,1);
+    Hx = _flux->GetEntry(i,3);
+    Hy = _flux->GetEntry(i,4);
+    zFlux += 0.5*real( Ex*conj(Hy) - conj(Ey)*Hx );
+  }
+  return zFlux/static_cast<double>( _nEvalPointsInEachDirection*_nEvalPointsInEachDirection );
+}
+    
+double FluxIntegrator::incidentFlux( scuff::RWGGeometry &geo, IncField &IF, double omega, double kBloch[2] )
+{
+  return this->compute( geo, &IF, NULL, omega, kBloch );
+}
+
+double FluxIntegrator::scatteredFlux( scuff::RWGGeometry &geo, HVector &vec, double omega, double kBloch[2] )
+{
+  return this->compute( geo, NULL, &vec, omega, kBloch );
+}
