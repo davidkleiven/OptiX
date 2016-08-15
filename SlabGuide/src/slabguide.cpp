48f7dfdc49cfb54ffcd43eed471e95ac3cbffe49
David Kleiven <davidkleiven446@gmail.com>
2016-08-15 08:41:10 +0200

    Finished the first executable version of the program

--- SlabGuide/src/slabguide.cpp
+++ SlabGuide/src/slabguide.cpp
@@ -20,6 +20,7 @@ const double YSIZE = 5.0;
 const double PML = 2.0;
 const double FREQ = 1.0;
 const double XSIZE = 10.0;
+const double RESOLUTION = 10.0;
 
 /* EPSION FUNCTION */
 double dielectric( const meep::vec &pos )
@@ -72,6 +73,24 @@ int main(int argc, char** argv)
   }
 
   // Define geometry
-  double resolution = 10.0;
+  meep::grid_volume vol = meep::vol2d( XSIZE, YSIZE, RESOLUTION );
+  const meep::symmetry sym = meep::mirror(meep::Y, vol);
+  meep::structure struc(vol, dielectric, meep::pml( PML ), sym);
+  meep::fields field(&struc);
+  field.set_output_directory( odir.c_str() );
+  meep::vec sourceLoc(PML, 0.0);
+  meep::continuous_src_time source(FREQ);
+  field.add_point_source( meep::Ez, source, sourceLoc ); 
+
+  // Compute time required to propagate over the entire domain
+  double tProp = XSIZE; // Speed of light c=1
+  
+  while ( field.time() < 1.5*tProp )
+  {
+    field.step();
+  }
+  
+  // Output field in the end
+  field.output_hdf5( meep::Ez, vol.surroundings() ); 
   return 0;
 }
