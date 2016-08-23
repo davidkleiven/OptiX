5050ff1e5ac0b6f3fc083a2fff45edb48a58c69d
David Kleiven <davidkleiven446@gmail.com>
2016-08-23 19:53:51 +0200

    Fixed label and added proper error plot

--- FresnelBEM/fluxPlot.py
+++ FresnelBEM/fluxPlot.py
@@ -16,8 +16,10 @@ def plotError( angle, error_rs, error_rp, error_ts, error_tp, brewster, fname ):
 
     ax.plot( angle, error_rs, 'o', ms=2, color='black', fillstyle='none', label="$R_\mathrm{s}$" )
     ax.plot( angle, error_rp, 'x', ms=2, color='black', label="$R_\mathrm{p}$")
-    ax.plot( angle, error_ts, '^', ms=2, color="black", label="$T_\mathrm{s}$")
-    ax.plot( angle, error_tp, '.', ms=4, color="black", label="$T_\mathrm{p}$")
+    if ( not error_ts is None ):
+        ax.plot( angle, error_ts, '^', ms=2, color="black", label="$T_\mathrm{s}$")
+    if ( not error_tp is None ):
+        ax.plot( angle, error_tp, '.', ms=4, color="black", label="$T_\mathrm{p}$")
     ax.set_ylim( 1E-11, 1E-1 )
     if ( np.min(angle) < brewster ):
         ax.axvline( brewster, color='black', ls="dotted" )
@@ -62,8 +64,8 @@ def main(argv):
 
     incangle = np.array( data["IncidentAngle"] )
     angle = np.linspace(np.min(incangle)-0.05, 90.0, 1001)
-    ax.plot( data["IncidentAngle"], data["FluxReflected"]["s"], 'o', ms=2, color='black', fillstyle='none', label="$R_\mathrm{s}$")
-    ax.plot( data["IncidentAngle"], data["FluxReflected"]["p"], 'x', ms=2, color='black', label="$R_\mathrm{p}$" )
+    ax.plot( data["IncidentAngle"], data["FluxReflected"]["s"], 'o', ms=3, color='black', fillstyle='none', label="$R_\mathrm{TE}$")
+    ax.plot( data["IncidentAngle"], data["FluxReflected"]["p"], 'x', ms=3, color='black', label="$R_\mathrm{TM}$" )
     if ( useAbsorption ):
         eps1 = 1.0
         eps2 = 0.99998*(1.0+2E-6*1j )
@@ -72,6 +74,7 @@ def main(argv):
         k = 0.01
         ax.plot(angle, fe.Rs(eps1, eps2, mu1, mu2, angle, k), color='black')
         ax.plot(angle, fe.Rp(eps1, eps2, mu1, mu2, angle, k), color='black')
+        ax.set_ylabel("Reflectance")
     else:
         ax.plot( data["IncidentAngle"], data["FluxTransmitted"]["s"], '^', ms=2, color='black', label="$T_\mathrm{s}$")
         ax.plot( data["IncidentAngle"], data["FluxTransmitted"]["p"], '.', ms=4, color='black', label="$T_\mathrm{p}$")
@@ -79,8 +82,8 @@ def main(argv):
         ax.plot(angle, pf.Rp(angle, n1, n2), color='black')
         ax.plot(angle, pf.Tp(angle, n1, n2), color='black')
         ax.plot(angle, pf.Ts(angle, n1, n2), color='black')
+        ax.set_ylabel("Transmittance/Reflectance")
     ax.set_xlabel("Incident angle")
-    ax.set_ylabel("Transmittance/Reflectance")
     ax.legend( loc='center left', frameon=False)
     fname = "Figures/powerCoefficients.pdf"
     fig.savefig( fname, bbox_inches="tight" )
@@ -89,14 +92,26 @@ def main(argv):
     # Plot relative error
     simulatedAngles = np.array( data["IncidentAngle"] )
     brewster = cra.brewster(n1,n2)*180.0/np.pi
-    Rs = pf.Rs(simulatedAngles, n1, n2)
-    Rp = pf.Rp(simulatedAngles, n1, n2)
-    Ts = pf.Ts(simulatedAngles, n1, n2)
-    Tp = pf.Tp(simulatedAngles, n1, n2)
+    if ( useAbsorption ):
+        Rs = fe.Rs(eps1,eps2,mu1,mu2,simulatedAngles,k)
+        Rp = fe.Rp(eps1,eps2,mu1,mu2,simulatedAngles,k)
+        Ts = None
+        Tp = None
+    else:
+        Rs = pf.Rs(simulatedAngles, n1, n2)
+        Rp = pf.Rp(simulatedAngles, n1, n2)
+        Ts = pf.Ts(simulatedAngles, n1, n2)
+        Tp = pf.Tp(simulatedAngles, n1, n2)
     errorRs = np.abs( np.array(data["FluxReflected"]["s"]) - Rs)/Rs
     errorRp = np.abs(np.array(data["FluxReflected"]["p"]) - Rp)/Rp
-    errorTs = np.abs( np.array(data["FluxTransmitted"]["s"]) - Ts )/Ts
-    errorTp = np.abs( np.array(data["FluxTransmitted"]["p"]) - Tp )/Tp
+    if ( Ts is None ):
+        errorTs = None
+    else:
+        errorTs = np.abs( np.array(data["FluxTransmitted"]["s"]) - Ts )/Ts
+    if ( Tp is None ):
+        errorTp = None
+    else:
+        errorTp = np.abs( np.array(data["FluxTransmitted"]["p"]) - Tp )/Tp
     plotError( simulatedAngles, errorRs, errorRp, errorTs, errorTp, brewster, "Figures/powerCoefficientsError.pdf" )
     
 if __name__ == '__main__':
