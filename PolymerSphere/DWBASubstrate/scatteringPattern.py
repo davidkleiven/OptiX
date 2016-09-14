a3b694a916d4fdc069c2536185ac100192ede51a
David Kleiven <davidkleiven446@gmail.com>
2016-09-14 14:56:19 +0200

    DWBA seems to work

--- PolymerSphere/DWBASubstrate/scatteringPattern.py
+++ PolymerSphere/DWBASubstrate/scatteringPattern.py
@@ -9,58 +9,49 @@ from matplotlib import pyplot as plt
 import json
 import fresnelExact as fe
 
-def Born1( incWaveVec, x, y, z, Rsphere ):
-    q_parallel = qPar( incWaveVec )
-    k_scattered_z = kScatteredPerp( incWaveVec, x, y, z )
-    q_norm = k_scattered_z - incWaveVec[2]
-    return formFactorSphere( q_parallel, q_norm, Rsphere )
- 
-def reflectedFromSubsrate( incWaveVec, x, y, z, Rsphere, epsSubst, pol="TE" ):
-    q_parallel = qPar( incWaveVec )
-    k_scattered_z = kScatteredPerp( incWaveVec, x, y, z )
-    q_norm = k_scattered_z + incWaveVec[2]
-    form = formFactorSphere( q_parallel, q_norm, Rsphere )
-    k = np.sqrt( np.sum(incWaveVec**2) )
-    if ( pol == "TE" ):
-        return form*fe.rs( 1.0, epsSubst, 1.0, 1.0, incAngle(incWaveVec), k )
-    return form*fe.rp( 1.0, epsSubst, 1.0, 1.0, incAngle(incWaveVec), k )
-
-def reflectedFromSubstrateAfter( incWaveVec, x, y, z, Rsphere, epsSubst, pol="TE" ):
-    q_parallel = qPar( incWaveVec )
-    k_scattered_z = kScatteredPerp( incWaveVec, x, y, z )
-    q_norm = -k_scattered_z - incWaveVec[2]
-    form = formFactorSphere( q_parallel, q_norm, Rsphere )
-    k = np.sqrt( np.sum(incWaveVec**2) )
-    angle = np.arccos(k_scattered_z/k)*180.0/np.pi
-    if ( pol == "TE" ):
-        return form*fe.rs( 1.0, epsSubst, 1.0, 1.0, angle, k )
-    return form*fe.rp( 1.0, epsSubst, 1.0, 1.0, angle, k )
     
-def reflectedFromSubsrateBeforeAndAfter( incWaveVec, x, y, z, Rsphere, epsSubst, pol="TE" ):
-    q_parallel = qPar( incWaveVec )
-    k_scattered_z = kScatteredPerp( incWaveVec, x, y, z )
-    q_norm = -k_scattered_z + incWaveVec[2]
-    form = formFactorSphere( q_parallel, q_norm, Rsphere )
-    k = np.sqrt( np.sum(incWaveVec**2) )
-    angle = np.arccos(k_scattered_z/k)*180.0/np.pi
-    if ( pol == "TE" ):
-        return form*fe.rs( 1.0, epsSubst, 1.0, 1.0, incAngle(incWaveVec), k )*fe.rs(1.0, epsSubst, 1.0,1.0, angle, k)
-    return form*fe.rp( 1.0, epsSubst, 1.0, 1.0, incAngle(incWaveVec), k )*fe.rp(1.0, epsSub, 1.0, 1.0, angle, k )
+def dwba( waveVector, x, y, z, eps, mode="Born" ):
+    k = np.sqrt( np.sum(waveVector**2) )
+    q_paralell = np.sqrt( waveVector[1]**2 + waveVector[2]**2 ) 
+    rHat_x = x/np.sqrt(x**2+y**2+z**2)
+    kx_scat = k*rHat_x
+    qx = kx_scat - waveVector[0]
+    qR = np.sqrt( q_paralell**2 + qx**2 )
+    qR[qx<0.0] = -qR[qx<0.0]
+    formBorn = formFactorSphere(qR)
+    if ( mode == "Born" ):
+        return formBorn
     
-def incAngle( incWaveVec ):
-    k = np.sqrt( np.sum(incWaveVec**2) )
-    return np.arccos( incWaveVec[2]/k )*180.0/np.pi
+    # Contrib from scattered before entering sphere
+    qx = kx_scat + waveVector[0]
+    qR = np.sqrt( q_paralell**2 + qx**2 )
+    angleWithSubstrateDeg = np.arccos(-waveVector[0]/k)*180.0/np.pi
+    firstDWBA = formFactorSphere(qR)*fe.rs(1.0,eps,1.0,1.0,angleWithSubstrateDeg, k)
+    if ( mode == "First" ):
+        return firstDWBA
 
-def qPar( incWaveVec ):
-    return np.sqrt(incWaveVec[0]**2 + incWaveVec[1]**2)
+    # Contrib from scattering after entering sphere
+    qx = -kx_scat - waveVector[0]
+    qR = np.sqrt( q_paralell**2 + qx**2 )
+    angleWithSubstrateDeg = np.arccos(kx_scat/k)*180.0/np.pi
+    secondDWBA = formFactorSphere(qR)*fe.rs(1.0,eps,1.0,1.0,angleWithSubstrateDeg,k)
+    secondDWBA[-kx_scat>0.0] = 0.0 # Ray leaves sphere with kx = -kx_scat
+    if ( mode == "Second" ):
+        return secondDWBA
 
-def kScatteredPerp( incWaveVec, x, y, z ):
-    k = np.sqrt( np.sum(incWaveVec**2) )
-    return k*np.sqrt(x**2 + y**2)/np.sqrt(x**2+y**2+z**2)
+    # Contibution from scattering before enterin sphere and after
+    qx = -kx_scat + waveVector[0]
+    qR = np.sqrt( q_paralell**2 + qx**2 )
+    angleWithSubstrateFirstDeg = np.arccos(np.abs(waveVector[0])/k)*180.0/np.pi
+    angleWithSubstrateSecondDeg = np.arccos(kx_scat/k)*180.0/np.pi
+    thirdDWBA = formFactorSphere(qR)*fe.rs(1.0,eps,1.0,1.0,angleWithSubstrateFirstDeg,k)*fe.rs(1.0,eps,1.0,1.0,angleWithSubstrateSecondDeg,k) 
+    thirdDWBA[-kx_scat>0.0] = 0.0
+    if ( mode == "Third" ):
+        return thirdDWBA
+    return formBorn + firstDWBA + secondDWBA + thirdDWBA
     
-def formFactorSphere( q_parallel, q_norm, Rsphere ):
-    qR = np.sqrt( q_parallel**2 + q_norm**2 )*Rsphere
-    qR[np.abs(qR) < 1E-5] = 1E-5
+    
+def formFactorSphere( qR ):
     form = ( np.sin(qR) - qR*np.cos(qR) )/qR**3
     return form
 
@@ -72,57 +63,43 @@ def grazingIncidence(grazingAngle, polarisation, epsSub):
     Rsphere = 1.0
     kR = 10.0
     k = np.zeros(3)
-    k[1] = -kR*np.sin(grazingAngle*alpha_c)
+    k[0] = -kR*np.sin(grazingAngle*alpha_c)
     k[2] = kR*np.cos(grazingAngle*alpha_c)
 
-    k_scattered = np.zeros(3)
-    k_scattered[1] = kR*np.sin(grazingAngle*alpha_c)
-    k_scattered[2] = kR*np.cos(grazingAngle*alpha_c)
-
     incAngle = 90.0 - grazingAngle*alpha_c*180.0/np.pi
-    if ( polarisation == "TE" ):
-        r = fe.rs( epsInc, epsSub, mu, mu, incAngle, kR )
-    elif ( polarisation == "TM" ):
-        r = fe.rp( epsInc, epsSub, mu, mu, incAngle, kR )
-    else:
-        print ("Unknown polarisation...")
-        return
-    
-    z = 1000.0
-    x = np.linspace( -z, z, 101 )
-    y = np.linspace( -z, z, 101 )
-    X, Y = np.meshgrid( x, y )
-    f1 = Born1( k, X, Y, z, Rsphere )
-    f2 = reflectedFromSubsrate( k_scattered, X, Y, z, Rsphere, epsSub )
-    f3 = reflectedFromSubstrateAfter( k_scattered, X, Y, z, Rsphere, epsSub )
-    f4 = reflectedFromSubsrateBeforeAndAfter( k_scattered, X, Y, z, Rsphere, epsSub )
-    f = np.abs(f1 + f2 + f3 + f4)**2
-    
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.contourf(X,Y,f,200, map="gist_heat")
-    
-    fname = "Figures/pattern.png"
-    fig.savefig( fname, bbox_inches="tight" )
-    print ("Figure written to %s"%(fname))
+    z = 100.0
+    x = np.linspace(0.0,2.5*z,10001)
+    born = dwba( k, x, 0.0, z, epsSub, mode="Born")
+    f1 = dwba( k, x, 0.0, z, epsSub, mode="First")
+    f2 = dwba( k, x, 0.0, z, epsSub, mode="Second")
+    f3 = dwba( k, x, 0.0, z, epsSub, mode="Third")
+    tot = dwba( k, x, 0.0, z, epsSub)
+    alpha_f = np.arctan(x/z)/alpha_c
 
-    f1 = Born1( k, x, 0.0, z, Rsphere )
-    f2 = reflectedFromSubsrate( k_scattered, x, 0.0, z, Rsphere, epsSub )
-    f3 = reflectedFromSubstrateAfter( k_scattered, x, 0.0, z, Rsphere, epsSub )
-    f4 = reflectedFromSubsrateBeforeAndAfter( k_scattered, x, 0.0, z, Rsphere, epsSub )
-    f = np.abs(f1 + f2 + f3 + f4)**2
-     
+    colors = ["#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99"]
+    markers = ['-o', '-v', '-s', '-h', '-d']
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
-    ax.plot( x, f1, 'k', label="Parallel" )
-    ax.set_xlabel("Distance from center of screen")
-    ax.legend(frameon=False)
+    ax.plot( alpha_f, np.abs(born)**2, color=colors[0], label="A")
+    ax.plot( alpha_f, np.abs(f1)**2, color=colors[1], label="B")
+    ax.plot( alpha_f, np.abs(f2)**2, color=colors[2], label="C")
+    ax.plot( alpha_f, np.abs(f3)**2, color=colors[3], label="D")
+    ax.set_yscale('log')
+    ax.set_xlabel("$\\alpha_f/\\alpha_c$")
+    ax.set_ylabel("Intensity (a.u.)")
+    if ( grazingAngle > 1.0 ):
+        ax.text( 0.8*alpha_f[-1], 1E-10, "$\\alpha_i = %d\\alpha_c$"%(grazingAngle))
+    elif( grazingAngle == 1 ):
+        ax.text( 0.8*alpha_f[-1], 1E-10, "$\\alpha_i = \\alpha_c$") 
+    else:
+        ax.text( 0.8*alpha_f[-1], 1E-10, "$\\alpha_i = %.1f\\alpha_c$"%(grazingAngle))
+    ax.legend( loc="lower right", frameon=False, ncol=4 )
     fname = "Figures/pattern1D.pdf"
     fig.savefig( fname, bbox_inches="tight" )
     print ("Figure written to %s"%(fname))
 
 def main():
-    grazingIncidence( 10.0, "TE", (1-1E-5+1j*1E-6)**2 )
+    grazingIncidence( 0.5, "TE", (1-1E-5+1j*1E-6)**2 )
 
 if __name__ == "__main__":
     main() 
