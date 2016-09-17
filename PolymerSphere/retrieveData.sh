1cc5245a641ff591355767abd46be06b0c4a9f0b
David Kleiven <davidkleiven446@gmail.com>
2016-09-17 10:51:08 +0100

    Copying only the current UID, and update of run notes

--- PolymerSphere/retrieveData.sh
+++ PolymerSphere/retrieveData.sh
@@ -1,4 +1,12 @@
 source servers.sh
 DESTINATION=data
 
-scp davidkl@${IP}:"${SERVER_PATH}/data/*.json ${SERVER_PATH}/data/*.bin ${SERVER_PATH}/data/*.pp ${SERVER_PATH}/data/*.h5" ${DESTINATION}
+key="$1"
+if [ "$key" != "--uid" ]
+then
+  echo "Unknown key $key "
+  exit 1
+fi
+
+uid="$2"
+scp davidkl@${IP}:"${SERVER_PATH}/data/*$uid*" ${DESTINATION}
