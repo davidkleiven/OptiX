3ab0709e9cc59ede787ff2d4cc0feb52a117c0c2
David Kleiven <davidkleiven446@gmail.com>
2016-09-30 10:44:14 +0200

    Added inverse transformation

--- PolymerSphere/DWBASubstrate/transformDetector.py
+++ PolymerSphere/DWBASubstrate/transformDetector.py
@@ -10,12 +10,18 @@ class DetectorTransformer:
     def axisLabel(self):
         return "$\\alpha_f/\\alpha_c$"
 
+    def alpha_f( self, incidentGrazingAngle, scattering ):
+        return scattering
+
 class DetectorCenterBeam(DetectorTransformer):
     def __init__(self):
         self.name = "detectorCenterBeam"
 
     def scatteringAngle(self, incidentGrazingAngle, grazingAngleFromSubstrate):
         return incidentGrazingAngle + grazingAngleFromSubstrate
+
+    def alpha_f( self, incidentGrazingAngle, scatteringAngle ):
+        return scatteringAngle - incidentGrazingAngle
     
     def axisLabel(self):
         return "$(\\alpha+\\alpha_f)/\\alpha_c$"
