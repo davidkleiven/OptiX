26bdacc767841e7587b4d6a3e92228d6dead10d6
David Kleiven <davidkleiven446@gmail.com>
2016-10-01 17:38:47 +0200

    Printing which source is used

--- PolymerSphere/sphereScattering.cpp
+++ PolymerSphere/sphereScattering.cpp
@@ -221,9 +221,12 @@ int main(int argc, char **argv)
 
   #ifndef USE_GAUSSIAN_BEAM
     PlaneWave incField(E0_s, kHat);
+    clog << "Using plane wave source\n";
   #else
     double beamCenter[3] = {0.0,0.0,0.0};
     double beamWaist = 3.0;
+    clog << "Using Gaussian beam source. Center: " << beamCenter[0] << ",";
+    clog << beamCenter[1] << "," << beamCenter[2] << ". W0=" << beamWaist << endl;
     GaussianBeam incField(beamCenter, kHat, E0_s, beamWaist);
   #endif
    
