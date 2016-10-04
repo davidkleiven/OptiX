818581f57660a53d032c1ab60c432e3c4b9435a5
David Kleiven <davidkleiven446@gmail.com>
2016-10-04 08:17:49 +0200

    Bash script for initializing the paths

---
+++ setupEnv.sh
@@ -0,0 +1,4 @@
+# This script sets up environmental variables for different servers
+LIB_PATH_U2=/media/davidkl/stud/Master/PreCompiledDebLib/ #Path to the .so files in the computer room BU2-023 (linux)
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIB_PATH_U2}
