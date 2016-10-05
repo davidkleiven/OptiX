51e8c447dd863160c415ddaa2cbc75535875d61d
David Kleiven <davidkleiven446@gmail.com>
2016-10-05 16:02:30 +0200

    Made the program much more flexible

--- PolymerSphere/DWBABornAgain/sphereSubstrate.cpp
+++ PolymerSphere/DWBABornAgain/sphereSubstrate.cpp
@@ -9,9 +9,56 @@
 #include "ParticleLayout.h"
 #include "Simulation.h"
 #include "Units.h"
+#include <string>
+#include <sstream>
+
+using namespace std;
+enum class Mode_t {ONLY_SPHERE, SPHERE_ON_PLANE, SPHERE_ON_MEMBRANE};
 
 int main(int argc, char **argv)
 {
+  string HELP_MSG("Usage: ./sphereSubstrate [arguments]\n");
+  HELP_MSG += "--help: Print this message\n";
+  HELP_MSG += "--sphere: Run simulation with a single sphere\n";
+  HELP_MSG += "--sphereOnPlane: Run simulation with a sphere on a plane\n";
+  HELP_MSG += "--sphereOnMembrane: Run simulation with a sphere on a membrane\n";
+  HELP_MSG += "--thick: Thickness of the membrane in units of the radius of the sphere\n";
+
+  // Parse arguments
+  Mode_t mode = Mode_t::ONLY_SPHERE;
+  double thicknessInUnitsOfRSphere = 0.1;
+  for ( unsigned int i=1;i<argc;i++ )
+  {
+    string arg( argv[i] );
+    if ( arg.find( "--sphere") != string::npos )
+    {
+      mode = Mode_t::ONLY_SPHERE;
+    }
+    else if ( arg.find("--sphereOnPlane") != string::npos )
+    {
+      mode = Mode_t::SPHERE_ON_PLANE;
+    }
+    else if ( arg.find("--sphereOnMembrane") != string::npos )
+    {
+      mode = Mode_t::SPHERE_ON_MEMBRANE;
+    }
+    else if ( arg.find("--thick=") != string::npos )
+    {
+      stringstream ss;
+      ss << arg.substr(7);
+      ss >> thicknessInUnitsOfRSphere;
+    }
+    else if ( arg.find("--help") != string::npos )
+    {
+      cout << HELP_MSG;
+      return 0;
+    }
+    else
+    {
+      cout << "Unknown argument " << arg << endl;
+      return 0;
+    }
+  }
   // Refractive index of substrate
   // n = 1-delta + i*beta
   double refractiveIndexSubstrateDelta = 1.5E-5;
@@ -31,19 +78,51 @@ int main(int argc, char **argv)
   HomogeneousMaterial particleMaterial("Polymer", refractiveIndexParticleDelta, refractiveIndexParticleBeta);
 
   // Define a sphere with radius 1 um
-  Particle sphere(particleMaterial, FormFactorFullSphere(1.0*Units::micrometer));
+  auto rSphere = 1.0*Units::micrometer;
+  Particle sphere(particleMaterial, FormFactorFullSphere(rSphere));
 
   partLayout.addParticle(sphere);
   airLayer.addLayout(partLayout);
 
   MultiLayer sample;
-  sample.addLayer(airLayer);
-  sample.addLayer(substrateLayer);
+
+  switch ( mode )
+  {
+    case Mode_t::ONLY_SPHERE:
+      clog << "Running with only sphere in air\n";
+      sample.addLayer(airLayer);
+      break;
+    case Mode_t::SPHERE_ON_PLANE:
+      clog << "Running with a sphere on a plane\n";
+      sample.addLayer(airLayer);
+      sample.addLayer(substrateLayer);
+    case Mode_t::SPHERE_ON_MEMBRANE:
+      clog << "Running with a sphere on a membrane of finite thickness\n";
+      sample.addLayer(airLayer);
+      substrateLayer.setThickness(thicknessInUnitsOfRSphere*rSphere);
+      sample.addLayer(substrateLayer);
+      sample.addLayer(airLayer);
+      break;
+  }
+
+  auto alpha_min = 0.0*Units::degree;
+  auto alpha_max = 0.9*Units::degree;
+  auto phi_min = -0.3*Units::degree;
+  auto phi_max = 0.3*Units::degree;
+  auto incidentGrazingAngle = 0.2*Units::degree;
+  switch ( mode )
+  {
+    case Mode_t::ONLY_SPHERE:
+      alpha_min = phi_min;
+      alpha_max = phi_max;
+      incidentGrazingAngle = 0.0*Units::degree;
+      break;
+  }
 
   // Setup Simulation
   GISASSimulation simulation;
-  simulation.setDetectorParameters(512, -0.3*Units::degree, 0.3*Units::degree, 512, 0.0*Units::degree, 0.9*Units::degree);
-  simulation.setBeamParameters(1.0*Units::angstrom, 0.2*Units::degree, 0.0*Units::degree);
+  simulation.setDetectorParameters(512, phi_min, phi_max, 512, alpha_min, alpha_max);
+  simulation.setBeamParameters(1.0*Units::angstrom, incidentGrazingAngle, 0.0*Units::degree);
   simulation.setSample(sample);
 
   // Run simulation
