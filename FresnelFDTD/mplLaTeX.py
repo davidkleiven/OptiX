58bfac0152b364647f5dded2cf41625468bf9900
David Kleiven <davidkleiven446@gmail.com>
2016-10-12 11:26:59 +0200

    Check if it can detect if a package exists

--- FresnelFDTD/mplLaTeX.py
+++ FresnelFDTD/mplLaTeX.py
@@ -4,7 +4,7 @@ if ( ONE_COLUMN ):
 else:
     figsize = [3.5, 2.25]
 params = {'backend': 'ps',
-       'text.latex.preamble': ['\usepackage{gensymb}\usepackage{siunitx}'],
+       'text.latex.preamble': ['\usepackage{gensymb}\IfFileExists{siunitx.sty}{\usepackage{siunitx}}{\providecommand{\SI}[2]{#1\mathrm{#2}}}'],
        'axes.labelsize': 8, # fontsize for x and y labels (was 10)
        'axes.titlesize': 8,
        'font.size': 8, # was 10
