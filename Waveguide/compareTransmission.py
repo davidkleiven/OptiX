89f3d557f9e0b9bd356a5e37d7a6b7f14847f503
David Kleiven <davidkleiven446@gmail.com>
2016-10-17 09:48:27 +0200

    Adding trendlines

--- Waveguide/compareTransmission.py
+++ Waveguide/compareTransmission.py
@@ -54,13 +54,20 @@ def main( argv ):
 
         data = data[::param["numberOfPoints"]]
         z = np.linspace(stat["Transmission"]["zStart"], stat["Transmission"]["zEnd"], len(data))
+        fitStart = int( len(data)/2 )
+        zFit = z[fitStart:]
+        dataFit = data[fitStart:]
+        slope, interscept, rvalue, pvalue, stderr = stat.linregress(zFit,np.log(dataFit))
         if ( stat["Transmission"]["zEnd"] < minOfMaxZ ):
             minOfMaxZ = stat["Transmission"]["zEnd"]
-        ax.plot( z/1E6, np.log(data), color="black", marker=markers[indx], ms=3, linestyle="None", label=entry["label"])
+            ymin = np.min(np.log(data))
+        ax.plot( z/1E6, np.log(data), color="black", marker=markers[indx], ms=2, linestyle="None", label=entry["label"])
+        ax.plot(z/1E6, interscept+slope*z/1E6, color="black", linestyle="--")
         indx += 1
 
     ax.set_xlabel("$z$ (mm)")
     ax.set_xlim(right=minOfMaxZ/1E6)
+    ax.set_ylim(lower=ymin-0.05)
     ax.set_ylabel("$\ln$ (Transmission)")
     ax.legend(loc="upper right", frameon=False)
     fname = "Figures/"+param["figurename"]
