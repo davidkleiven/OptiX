0efed8b747193ad8acc10f6ca79ab792b0f78e9f
David Kleiven <davidkleiven446@gmail.com>
2016-10-17 10:02:17 +0200

    Minor change

--- Waveguide/compareTransmission.py
+++ Waveguide/compareTransmission.py
@@ -67,7 +67,7 @@ def main( argv ):
             minOfMaxZ = stat["Transmission"]["zEnd"]
             ymin = np.min(np.log(data))
         ax.plot( z/1E6, np.log(data), color=cs.COLORS[indx], marker='.', ms=4, linestyle="None", label=entry["label"])
-        ax.plot( z/1E6, interscept+slope*z color="#C0C0C0" )
+        ax.plot( z/1E6, interscept+slope*z, color="#C0C0C0" )
         indx += 1
 
     ax.set_xlabel("$z$ (mm)")
