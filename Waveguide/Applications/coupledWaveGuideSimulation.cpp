e999e1986c285a7052b49aa53e9271577b350076
David Kleiven <davidkleiven446@gmail.com>
2016-10-19 20:55:02 +0200

    Can now simulate coupled curved waveguides

---
+++ Waveguide/Applications/coupledWaveGuideSimulation.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include "cladding.hpp"
+#include "curvedWaveGuide2D.hpp"
+#include "crankNicholson.hpp"
+#include "controlFile.hpp"
+#include "straightWG2D.hpp"
+#include "coupledCurvedWG.hpp"
+#include <complex>
+#include <stdexcept>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <sstream>
+
+using namespace std;
+
+typedef complex<double> cdouble;
+
+int main( int argc, char **argv )
+{
+  double R = 80.0;
+
+  // Parameters for running a sweep over radii of curvature
+  double LzOverR = 0.01; // max(z)/R << 1 is a requirement
+  double xMarginAboveAndBelow = 0.5E3; // In nanometers = 0.5 um
+  unsigned int Nz = 5000; // Number of discretization points in x and z direction
+  unsigned int nPointsTransmission = 200;
+
+  Cladding cladding;
+  double delta = 4.49E-5;
+  double beta = 3.45E-6;
+  cladding.setRefractiveIndex(delta, beta);
+  double width = 100.0; // Width of the waveguide in nm
+
+  // Start sweep
+
+  double Rcurv = R*1E6;
+  double zmin = 0.0;
+  double zmax = Rcurv*LzOverR;
+  double xmax = width+xMarginAboveAndBelow;
+  double xmin = -0.5*zmax*LzOverR-xMarginAboveAndBelow;
+
+  double stepX = (xmax-xmin)/static_cast<double>(Nz);
+  double stepZ = (zmax-zmin)/static_cast<double>(Nz);
+  stepX = stepX > 1.0 ? 1.0:stepX;
+  stepZ = stepZ > 100.0 ? 100.0:stepZ;
+
+  ControlFile ctl("data/coupledCurvedWG"); // File for all parameters and settings
+  CoupledCurvedWG wg;
+  try
+  {
+    clog << "Initializing simulation...";
+
+    wg.getWg1().setRadiusOfCurvature( Rcurv );
+    wg.getWg2().setRadiusOfCurvature( Rcurv*2.0 );
+    wg.getWg1().setWidth( width );
+    wg.getWg2().setWidth( width );
+
+    wg.setWaveLength( 0.1569 );
+    wg.setCladding( cladding );
+    wg.setTransverseDiscretization(xmin,xmax,stepX);
+    wg.setLongitudinalDiscretization(zmin,zmax,stepZ);
+    wg.setSeparation( 150.0 );
+    wg.setStartCoupler( 50E3 );
+
+    CrankNicholson solver;
+    wg.setSolver(solver);
+    wg.setBoundaryConditions();
+    clog << " done\n";
+    clog << "Solving linear system... ";
+    wg.solve();
+    clog << "done\n";
+    /*
+    clog << "Computing transmission... ";
+    wg->computeTransmission( (zmax-zmin)/static_cast<double>(nPointsTransmission) );
+    clog << "done\n";
+    */
+    clog << "Exporting results...\n";
+    wg.save( ctl );
+    //wg->saveTransmission( ctl );
+    ctl.save();
+    clog << "Finished exporting\n";
+  }
+  catch ( exception &exc )
+  {
+    cerr << exc.what() << endl;
+    return 1;
+  }
+  catch (...)
+  {
+    cerr << "An unrecognized exception occured!\n";
+    return 1;
+  }
+
+  clog << "The simulation ended successfully\n";
+  return 0;
+}
--- Waveguide/runNotes.md
+++ Waveguide/runNotes.md
@@ -61,4 +61,8 @@
 945091 | R = 100 mm | Looks OK |
 521840 | R = 150 mm | Looks OK |
 %%% | %%% | %%%
-281636 | 1D eigenmode run. R=40mm. delta=4.49E-5 | Not analysed | 
+281636 | 1D eigenmode run. R=40mm. delta=4.49E-5 | Not analysed |
+754680 | Coupled. R=80mm. delta=4.49E-5, sep=50 nm, couplerStart=1um | Difficult to see the second wg on log scale |
+363907 | Coupled. R=80mm. delta=4.49E-5, sep=150 nm, couplerStart=1um | Good, need to close the upper for a longer time |
+305415 | Coupled. R=80mm. delta=4.49E-5, sep=150 nm, couplerStart=50um | Ok, difficult to see |
+379863 | Coupled. R=80mm, R2=2R1. delta=4.49E-5, sep=150 nm, couplerStart=50um | Running |
