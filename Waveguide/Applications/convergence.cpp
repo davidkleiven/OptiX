44d520eb150968367655671a3583726591d956d7
David Kleiven <davidkleiven446@gmail.com>
2016-11-09 15:55:05 +0100

    Updated the convergence study

--- Waveguide/Applications/convergence.cpp
+++ Waveguide/Applications/convergence.cpp
@@ -4,6 +4,8 @@
 #include "crankNicholson.hpp"
 #include "controlFile.hpp"
 #include "straightWG2D.hpp"
+#include "paraxialEquation.hpp"
+#include "planeWave.hpp"
 #include <vector>
 #include <H5Cpp.h>
 #include <hdf5_hl.h>
@@ -21,6 +23,30 @@ vector<double> errorRatio( const vector<double> &solutionAtTest )
   return error;
 }
 
+double compare( const WaveGuideFDSimulation &poorDisc, const WaveGuideFDSimulation &goodDisc )
+{
+  // Loop over the matrix with fewest entries
+  unsigned int Nr = poorDisc.nodeNumberTransverse();
+  unsigned int Nc = poorDisc.nodeNumberLongitudinal();
+  double err1Sq = 0.0;
+  double err2Sq = 0.0;
+  double crossTerm = 0.0;
+  for ( unsigned int ic=0;ic<Nc;ic++ )
+  {
+    for ( unsigned int ir=0;ir<Nr;ir++ )
+    {
+      double err1 = abs( poorDisc.getSolver().getSolution()(ir,ic) );
+      double x = poorDisc.getX(ir);
+      double z = poorDisc.getZ(ic);
+      double err2 = sqrt( goodDisc.getIntensity(x,z) );
+      err1Sq += err1*err1;
+      err1Sq += err2*err2;
+      crossTerm += 2.0*err1*err2;
+    }
+  }
+  return sqrt( err1Sq+err2Sq - crossTerm);
+}
+
 int main( int argc, char** argv )
 {
   Cladding cladding1;
@@ -34,60 +60,65 @@ int main( int argc, char** argv )
   double xmin = -0.2E3;
   double xmax = width + 0.2E3;
 
-  const unsigned int Nx = 6;
-  const unsigned int Nz = 6;
-  double stepX[Nx] = {4.0, 2.0, 1.0, 0.5, 0.25, 0.125};
-  double stepZ[Nz] = {400.0, 200.0, 100.0, 50.0, 25.0, 12.5};
+  const unsigned int nElem = 7;
+  int N[nElem] = {100, 200, 400, 800, 1200, 2400, 4800}; // Not unsigned, due to HDF5
 
   StraightWG2D wg1;
+  StraightWG2D wg2;
   double testZ = zmax/2.0;
   double testX = width/2.0;
+  ParaxialEquation eq1;
+  ParaxialEquation eq2;
   CrankNicholson solver;
+  CrankNicholson solver2;
+  PlaneWave pw1;
+  PlaneWave pw2;
+  pw1.setWavelength( 0.1569 );
+  pw2.setWavelength( 0.1569 );
+  solver.setEquation(eq1);
+  solver2.setEquation(eq2);
   wg1.setWidth( width );
   wg1.setWaveLength( 0.1569 );
   wg1.setCladding( cladding1 );
+  wg2.setWidth( width );
+  wg2.setWaveLength( 0.1569 );
+  wg2.setCladding( cladding1 );
 
   // Convergence when varying dx
-  vector<double> solutionAtTestPointX;
+  vector<double> error;
   clog << "Varying stepsize in x-direction...\n";
-  for ( unsigned int i=0;i<Nx;i++ )
+  for ( unsigned int i=0;i<nElem-1;i++ )
   {
-    clog << i+1 << " of " << Nx << endl;
-    wg1.setTransverseDiscretization(xmin,xmax,stepX[i]);
-    wg1.setLongitudinalDiscretization(zmin,zmax,stepZ[2]);
+    clog << i+1 << " of " << nElem-1 << endl;
+    double stepX1 = (xmax-xmin)/static_cast<double>(N[i]);
+    double stepZ1 = (zmax-zmin)/static_cast<double>(N[i]);
+    double stepX2 = (xmax-xmin)/static_cast<double>(N[i+1]);
+    double stepZ2 = (zmax-zmin)/static_cast<double>(N[i+1]);
+    wg1.setTransverseDiscretization(xmin,xmax,stepX1);
+    wg1.setLongitudinalDiscretization(zmin,zmax,stepZ1);
     wg1.setSolver(solver); // Need to set the solver to ensure after changing the discretization
-    wg1.setBoundaryConditions();
+    wg1.setBoundaryConditions(pw1);
     wg1.solve();
-    solutionAtTestPointX.push_back( wg1.getIntensity(testX, testZ) );
-  }
 
-  vector<double> solutionAtTestPointZ;
-  clog << "Varying stepsize in z-direction...\n";
-  for ( unsigned int i=0;i<Nz;i++ )
-  {
-    clog << i+1 << " of " << Nz << endl;
-    wg1.setTransverseDiscretization(xmin,xmax,stepX[2]);
-    wg1.setLongitudinalDiscretization(zmin,zmax,stepZ[i]);
-    wg1.setSolver(solver); // Need to set the solver to ensure after changing the discretization
-    wg1.setBoundaryConditions();
-    wg1.solve();
-    solutionAtTestPointZ.push_back( wg1.getIntensity(testX, testZ) );
+    wg2.setTransverseDiscretization(xmin, xmax, stepX2);
+    wg2.setLongitudinalDiscretization(zmin, zmax, stepZ2);
+    wg2.setSolver(solver2);
+    wg2.setBoundaryConditions(pw2);
+    wg2.solve();
+    error.push_back( compare(wg1,wg2) );
   }
-
-  vector<double> errorX = errorRatio( solutionAtTestPointX );
-  vector<double> errorZ = errorRatio( solutionAtTestPointZ );
 
   string fname("data/convergence.h5");
-  hsize_t dim = errorX.size();
+  hsize_t dim = error.size();
   int rank = 1;
   hid_t file_id = H5Fcreate(fname.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT );
-  H5LTmake_dataset( file_id, "errorRatioX", rank, &dim, H5T_NATIVE_DOUBLE, &errorX[0]);
-  dim = Nx;
-  H5LTmake_dataset( file_id, "stepsizeX", rank, &dim, H5T_NATIVE_DOUBLE, stepX);
-  dim = errorZ.size();
-  H5LTmake_dataset( file_id, "errorRatioZ", rank, &dim, H5T_NATIVE_DOUBLE, &errorZ[0]);
-  dim = Nz;
-  H5LTmake_dataset( file_id, "stepsizeZ", rank, &dim, H5T_NATIVE_DOUBLE, stepZ);
+  H5LTmake_dataset( file_id, "error", rank, &dim, H5T_NATIVE_DOUBLE, &error[0]);
+  H5LTset_attribute_double( file_id, "error", "xmin", &xmin, 1);
+  H5LTset_attribute_double( file_id, "error", "xmax", &xmax, 1);
+  H5LTset_attribute_double( file_id, "error", "zmin", &zmin, 1);
+  H5LTset_attribute_double( file_id, "error", "zmax", &zmax, 1);
+  dim = nElem;
+  H5LTmake_dataset( file_id, "nodes", rank, &dim, H5T_NATIVE_INT, N);
   H5Fclose(file_id);
   clog << "Convergence data writte to " << fname << endl;
   return 0;
