c3e276285da177779736a6f1f1e27c45d538716f
David Kleiven <davidkleiven446@gmail.com>
2016-11-30 18:44:41 +0000

    Using the refractive indices instead of the Thomas screen length for WG potential

--- Waveguide/src/waveGuideStraight.cpp
+++ Waveguide/src/waveGuideStraight.cpp
@@ -8,7 +8,12 @@ double WaveGuideStraight::potential( double x ) const
 {
   if (( x > 0.0 ) || ( x < -width ))
   {
-    return cladding->getPotential();
+    //return cladding->getPotential();
+    return 2.0*cladding->getDelta()*wavenumber*wavenumber;
+  }
+  if ( core != NULL )
+  {
+    return 2.0*core->getDelta()*wavenumber*wavenumber;
   }
   return 0.0;
 }
