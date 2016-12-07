1c318ec9b16e968cead529b9123c1b727fb5b0ae
David Kleiven <davidkl@nt-07758.win.ntnu.no>
2016-12-07 09:29:51 +0100

    Transmission convergence

--- Waveguide/Scripting/transmissionConvergence.py
+++ Waveguide/Scripting/transmissionConvergence.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import numpy as np
 import matplotlib as mpl
+mpl.rcParams["svg.fonttype"] = "none"
+mpl.rcParams["font.size"] = 28
 from matplotlib import pyplot as plt
 import h5py as h5
 import subprocess
@@ -36,29 +38,52 @@ def main( argv ):
     values = []
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
+    setLabels = True
+    colors = ["#e41a1c", "#377eb8", "#4daf4a", "#984ea3"]
     for entry in data["dsets"]:
         simulations = {}
         for pos in positions:
             simulations[str(int(pos))] = Data()
 
         for uid in entry["uids"]:
-            fname = data["basename"]+entry["uid"]+".json"
+            fname = data["basename"]+str(uid)+".json"
             infile = open( fname, 'r')
             ctl = json.load( infile )
             infile.close()
 
             with h5.File( ctl["Transmission"]["file"], 'r' ) as hf:
                 T = np.array( hf.get("transmission") )
-            z = np.linspace( ctl["zDiscretization"]["min"], ctl["zDiscretization"]["max"], len(T) )
+            z = np.linspace( ctl["zDiscretization"]["min"], ctl["zDiscretization"]["max"], len(T) )/1000.0
+            if ( ctl["waveguide"]["crd"] == "cylindrical" ):
+                z *= ctl["waveguide"]["RadiusOfCurvature"]
+
             for pos in positions:
                 indx = np.argmin( np.abs(z-float(pos)) )
                 simulations[str(int(pos))].dx.append(ctl["xDiscretization"]["step"])
                 simulations[str(int(pos))].T.append(T[indx])
+        indx = 0
         for pos in positions:
             dx = np.array( simulations[str(int(pos))].dx )
             T = np.array( simulations[str(int(pos))].T )
-            ax.plot( 1.0/dx, T )
+            color = colors[indx%len(colors)]
+            if ( setLabels ):
+                ax.plot( 1.0/dx, np.log(T), label="%.2f mm"%(pos/1000.0), color=color, ls=entry["ls"], marker=entry["marker"], ms=7, fillstyle="none" )
+            else:
+                ax.plot( 1.0/dx, np.log(T), ls=entry["ls"], color=color, marker=entry["marker"], ms=7, fillstyle="none" )
+            indx += 1
+        setLabels = False
+    ax.legend(bbox_to_anchor=[1.05,0.45], frameon=False, labelspacing=0.005)
+    ax.set_xlabel("\$\Delta x^{-1}\$ (nm)\$^{-1}\$")
+    ax.set_ylabel("\$\log T\$")
     plt.show()
 
+    # Save figure
+    figname = data["figname"]
+    fig.savefig(figname)
+    print ("Figure written to %s"%(figname))
+    if ( figname[-3:] == "svg" ):
+        psname = figname[:-3]+"ps"
+        subprocess.call( ["inkscape", "--export-ps=%s"%(psname), "--export-latex", figname] )
+        print("Figure written to %s"%(psname))
 if __name__ == "__main__":
     main( sys.argv[1:] )
