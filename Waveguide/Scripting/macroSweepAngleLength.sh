6bd2b93585e6ba478b51a7eeb58ef7eeab4e5ae7
David Kleiven <davidkleiven446@gmail.com>
2016-12-15 16:15:13 +0000

    Perform sweep in a lower range

--- Waveguide/Scripting/macroSweepAngleLength.sh
+++ Waveguide/Scripting/macroSweepAngleLength.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/bash
-LENGTHS=(3.0 2.95 2.9 2.85 2.8 2.75 2.7 2.65 2.6 2.55 2.5 2.45 2.4 2.35 2.3 2.25 2.2 2.15 2.1 2.05 2.0)
+LENGTHS=(1.15 1.14 1.13 1.12 1.11 1.1 1.09 1.08 1.07 1.06 1.05 1.04 1.03 1.02 1.0 0.99 0.98 0.97 0.96 0.95 0.94 0.93 0.92 0.91 0.9)
 DFOLDER="lengthSweep"
 mkdir -p ${DFOLDER}
 i=0
 for wglength in ${LENGTHS[@]}
 do
   ./incidentAngleSweep.out --wglength=${wglength} --dfolder=${DFOLDER} --noUID --visualize
+  python incAngleSweep.py --file="${DFOLDER}/angleSweep.h5" --noshow --figname="${DFOLDER}/frame${i}.png" --title="L=$wglength"
+  i=$((i+1))
+done
+
+for wglength in ${LENGTHS[@]}
+do
+  ./incidentAngleSweep.out --wglength=${wglength} --dfolder=${DFOLDER} --noUID --visualize --usealc
   python incAngleSweep.py --file="${DFOLDER}/angleSweep.h5" --noshow --figname="${DFOLDER}/frame${i}.png" --title="L=$wglength"
   i=$((i+1))
 done
