eb7a74420cdcbb42c26a97d677aa76f201cd3897
David Kleiven <davidkleiven446@gmail.com>
2016-12-30 08:11:48 +0100

    Importing print function if python2

--- Waveguide/Scripting/compareH5Files.py
+++ Waveguide/Scripting/compareH5Files.py
@@ -1,6 +1,8 @@
 import numpy as np
 import h5py as h5
 import sys
+if ( sys.version_info[0] == 2 ):
+    from __future__ import print_function
 
 def compareLists( l1, l2 ):
     if ( len(l1) != len(l2) ):
