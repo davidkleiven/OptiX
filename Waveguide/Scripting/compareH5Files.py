d0c7564054cf3fcf84b57b0a39c2429b97fa111d
David Kleiven <davidkleiven446@gmail.com>
2016-12-30 08:14:33 +0100

    Minor change

--- Waveguide/Scripting/compareH5Files.py
+++ Waveguide/Scripting/compareH5Files.py
@@ -1,8 +1,7 @@
+from __future__ import print_function
 import numpy as np
 import h5py as h5
 import sys
-if ( sys.version_info[0] == 2 ):
-    from __future__ import print_function
 
 def compareLists( l1, l2 ):
     if ( len(l1) != len(l2) ):
