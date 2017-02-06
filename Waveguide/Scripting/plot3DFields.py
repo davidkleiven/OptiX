626df3b82566830318dc4088dca0f019423c3640
David Kleiven <davidkleiven446@gmail.com>
2017-02-06 11:08:59 +0100

    Minor change

--- Waveguide/Scripting/plot3DFields.py
+++ Waveguide/Scripting/plot3DFields.py
@@ -57,11 +57,9 @@ def main( argv ):
     control = ffPlot.projectionX( control )
     control = ffPlot.projectionY( control )
 
-    '''
     control = phasePlot.projectionXY( control )
     control = phasePlot.projectionX( control )
     control =phasePlot.projectionY( control )
-    '''
 
     control = intensityPlot.projectionX( control )
     root.mainloop()
