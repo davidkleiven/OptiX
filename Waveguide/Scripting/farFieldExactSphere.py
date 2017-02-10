dd89d14dd99e45e0753b1de24dc7aa1734737d84
David Kleiven <davidkleiven446@gmail.com>
2017-02-10 16:07:22 +0100

    Optionally plots the Airy function

--- Waveguide/Scripting/farFieldExactSphere.py
+++ Waveguide/Scripting/farFieldExactSphere.py
@@ -1,3 +1,4 @@
+import sys
 import proj3D
 import h5py as h5
 from PLOD import controlGUI as cg
@@ -22,7 +23,14 @@ def integrandReal( r, q ):
 def integrandImag( r, q ):
     return r*spec.jn( 0, q*r )*np.sin(-delta*k*np.sqrt(R**2 - r**2) )
 
-def main():
+def formFactorCircularStop( q ):
+    return 2.0*spec.jn( 1, q*R )/(q*R )
+
+def main( argv ):
+    plotCircularStop = 0
+    if ( len(argv) > 0 ):
+        plotCircularStop = argv[0]
+
     lim = proj3D.Limits()
     ffPlot = proj3D.FarField()
     with h5.File(fname, 'r') as hf:
@@ -44,6 +52,9 @@ def main():
     F = formsphere( q )**2
     F *= ( amp/np.max(F) )
 
+    Fc = formFactorCircularStop( q )**2
+    Fc *= ( amp/np.max(Fc) )
+
     Fphase = np.zeros(len(q)) + 1j*np.zeros(len(q))
     for i in range(0,len(q)):
         Fphase[i] = integrate.quad( integrandReal, 0.0, R, args=(q[i],))[0] + 1j*integrate.quad( integrandImag, 0.0, R, args=(q[i],))[0]
@@ -51,7 +62,11 @@ def main():
 
     control.plots.axes[0].ax.plot( q, F, color="#ca0020", label=r"\$F(q)\$")
     control.plots.axes[0].ax.legend( loc="upper left", frameon=False, labelspacing=0.01, borderpad=0.0, handletextpad=0.3, handlelength=0.1 )
+
+    if ( plotCircularStop == 1 ):
+        control.plots.axes[0].ax.plot( q, Fc, color="#4daf4a", label=r"\$F_c(q)\$")
+    control.plots.axes[0].ax.legend( loc="upper left", frameon=False, labelspacing=0.01, borderpad=0.0, handletextpad=0.3, handlelength=0.1 )
     root.mainloop()
 
 if __name__ == "__main__":
-    main()
+    main( sys.argv[1:] )
