f708240c09fa2d2159bf1b6d8ae2eb4292328fb0
David Kleiven <davidkleiven446@gmail.com>
2017-03-14 17:51:50 +0100

    Fixed error in exporting data

--- Coccolith/scatteringCrossSectionSphere.cpp
+++ Coccolith/scatteringCrossSectionSphere.cpp
@@ -9,20 +9,19 @@ typedef complex<double> cdouble;
 // Global variables
 meep::h5file *file = NULL;
 double radius = 3.0;
-unsigned int numberOfFreq = 400;
+unsigned int numberOfFreq = 200;
 volatile bool overwriteFile = true;
 double freq = 0.4, fwidth = 0.1;
 double pmlThick = 3.0/freq;
-double Nx = 2.0*(4.0*radius+pmlThick);
-double Ny = 2.0*(4.0*radius+pmlThick);
-double Nz = 2.0*(4.0*radius+pmlThick);
+double Nx = 2.0*(1.5*radius+pmlThick);
+double Ny = 2.0*(1.5*radius+pmlThick);
+double Nz = 2.0*(1.5*radius+pmlThick);
 
 double eps( const meep::vec&r )
 {
   if ( pow(r.x()-Nx/2.0,2) + pow(r.y()-Ny/2.0,2) + pow(r.z()-Nz/2.0,2) < radius*radius )
   {
-    //return 3.0;
-    return -1E12;
+    return 2.0;
   }
   return 1.0;
 }
@@ -39,7 +38,7 @@ double epsRef( const meep::vec &r )
 
 void run( double (*eps)(const meep::vec &r) )
 {
-  double resolution = 5.0; // pixels per distance
+  double resolution = 8.0; // pixels per distance
   meep::grid_volume v = meep::vol3d(Nx,Ny,Nz, resolution);
   meep::structure struc(v, eps, meep::pml(pmlThick));
   meep::fields f(&struc);
@@ -67,11 +66,11 @@ void run( double (*eps)(const meep::vec &r) )
     //meep::dft_flux transPlane = f.add_dft_flux_plane( transPlaneVol, freq-fwidth/2.0, freq+fwidth/2.0, numberOfFreq );
     //meep::dft_flux fluxBox = f.add_dft_flux_box( fluxBoxVol, freq-fwidth/2.0, freq+fwidth/2.0, numberOfFreq );
 
-    meep::vec refCrn1( Nx/2.0-radius-1.0, pmlThick, pmlThick );
-    meep::vec refCrn2( Nx/2.0-radius-1.0, Ny-pmlThick-1.0, Nz-pmlThick-1.0);
+    meep::vec refCrn1( pmlThick+2.0, pmlThick, pmlThick );
+    meep::vec refCrn2( pmlThick+2.0, Ny-pmlThick-1.0, Nz-pmlThick-1.0);
     meep::volume refPlaneVol(refCrn1, refCrn2);
-    meep::vec boxEndCrn( Nx/2.0+radius+1.0, Ny-pmlThick-1.0, Nz-pmlThick-1.0);
-    meep::vec transPlaneCrn( Nx/2.0+radius+1.0, pmlThick, pmlThick );
+    meep::vec boxEndCrn( Nx-pmlThick-1.0, Ny-pmlThick-1.0, Nz-pmlThick-1.0);
+    meep::vec transPlaneCrn( Nx-pmlThick-1.0, pmlThick, pmlThick );
     meep::volume fluxBoxVol( refCrn1, boxEndCrn );
     meep::volume transPlaneVol( transPlaneCrn, boxEndCrn );
     meep::dft_flux refPlane = f.add_dft_flux_plane( refPlaneVol, freq-fwidth/2.0, freq+fwidth/2.0, numberOfFreq );
@@ -96,7 +95,7 @@ void run( double (*eps)(const meep::vec &r) )
     meep::master_printf("Time to propagate %.2f\n", timeToProp );
     meep::master_printf("End time: %.2f\n", endTime);
   #else
-    double endTime = timeToProp;
+    double endTime = 50.0*Nz;
   #endif
 
   while ( f.time() < endTime)
@@ -125,7 +124,7 @@ void run( double (*eps)(const meep::vec &r) )
       file->prevent_deadlock();
 
       double *fluxTrans = transPlane.flux();
-      file->write("transFluxRef", 1, &Nfreq, fluxRef, false );
+      file->write("transFluxRef", 1, &Nfreq, fluxTrans, false );
       file->prevent_deadlock();
 
       double *fluxOut = fluxBox.flux();
@@ -138,6 +137,12 @@ void run( double (*eps)(const meep::vec &r) )
       double crnRefPlane[nCrn] = {minCrn.x(), minCrn.y(), minCrn.z(), maxCrn.x(), maxCrn.y(), maxCrn.z()};
       file->write( "refPlaneCrd", 1, &nCrn, crnRefPlane, false );
       file->prevent_deadlock();
+
+      minCrn = fluxBoxVol.get_min_corner();
+      maxCrn = fluxBoxVol.get_max_corner();
+      double crnFluxBox[nCrn] = {minCrn.x(),minCrn.y(),minCrn.z(),maxCrn.x(),maxCrn.y(),maxCrn.z()};
+      file->write( "fluxBoxCrn", 1, &nCrn, crnFluxBox, false );
+      file->prevent_deadlock();
     }
     else
     {
@@ -148,7 +153,7 @@ void run( double (*eps)(const meep::vec &r) )
       file->prevent_deadlock();
 
       double *fluxTrans = transPlane.flux();
-      file->write("transFluxScat", 1, &Nfreq, fluxRef, false );
+      file->write("transFluxScat", 1, &Nfreq, fluxTrans, false );
       file->prevent_deadlock();
 
       double *fluxOut = fluxBox.flux();
