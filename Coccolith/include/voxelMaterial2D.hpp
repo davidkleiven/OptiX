a816c4f77eea0f48ceb1efe433cba5396cb67b34
David Kleiven <davidkleiven446@gmail.com>
2017-03-28 12:44:54 +0200

    Class for 2D voxel material structure

---
+++ Coccolith/include/voxelMaterial2D.hpp
@@ -0,0 +1,91 @@
+#ifndef VOXELMATERIAL_2D_H
+#define VOXELMATERIAL_2D_H
+#include <meep.hpp>
+#include <armadillo>
+#include <string>
+
+struct FileInfo
+{
+  double resolutionNM{1.0};
+  unsigned int Nx{0};
+  unsigned int Ny{0};
+};
+
+struct ComputationalDomain
+{
+  double xmin{0.0};
+  double xmax{1.0};
+  double ymin{0.0};
+  double ymax{1.0};
+};
+
+/** Class for handling input from 2D raw binary files */
+class VoxelMaterial2D: public meep::material_function
+{
+public:
+  VoxelMaterial2D(){};
+
+  /** Loads raw voxel file */
+  void loadRaw( const char* fname );
+
+  /** Loads raw voxel file */
+  void loadRaw( const std::string& fname );
+
+  /** Number of nodes in the x-direction */
+  unsigned int Nx() const{ return voxels.n_rows; };
+
+  /** Number of nodes in the y-direction */
+  unsigned int Ny() const { return voxels.n_cols; };
+
+  /** Returns the voxel value at index (x,y) */
+  unsigned char get( unsigned int x, unsigned int y ) const{ return voxels(x,y); };
+
+  /** Get the voxel value at position r */
+  unsigned char get( const meep::vec &r ) const;
+
+  /** True if the vector is inside the structure */
+  bool isInside( const meep::vec &r ) const;
+
+  /** Updates the structure if using dispersive materials */
+  virtual void updateStructure(){};
+
+  // ====================== PUBLIC ATTRIBUTES ==================================
+  /** Threshold when determining if a value is inside the material or not */
+  unsigned char threshold{128};
+
+  /** This structure occupies the domain described by domain */
+  ComputationalDomain domain;
+protected:
+  static arma::Mat<unsigned char> voxels;
+
+  /** Extracts, resoluion and dimensions from the filename */
+  static void extractInfoFromFilename( const std::string& fname, FileInfo &info );
+
+  /** Applies threshold. 1 if the voxel value is larger than threshold, zero otherwise */
+  void applyThreshold();
+
+  /** True if the point is outside the computational domain */
+  bool isOutsideComputationalDomain( const meep::vec &r ) const;
+};
+
+// ================== DISPERSIVE VOXEL 2D MATERIAL =============================
+class Voxel2DSusceptibility: public VoxelMaterial2D
+{
+public:
+  Voxel2DSusceptibility( double sigma ): sigma(sigma){};
+  Voxel2DSusceptibility( double sigma, double defaultParam ): sigma(sigma), defaultParam(defaultParam){};
+
+  virtual void sigma_row(meep::component c, double sigrow[3], const meep::vec &r) override;
+  virtual double chi1p1(meep::field_type ft, const meep::vec &r) override { (void)ft; return f(r); }
+  virtual double eps(const meep::vec &r) override { return f(r); }
+  virtual double mu(const meep::vec &r) override { return f(r); }
+  virtual double conductivity(meep::component c, const meep::vec &r) override {
+  (void)c; return f(r); }
+  virtual double chi3(meep::component c, const meep::vec &r) override { (void)c; return f(r); }
+  virtual double chi2(meep::component c, const meep::vec &r) override { (void)c; return f(r); }
+private:
+  double f( const meep::vec &r );
+  double sigma{0.0};
+  double defaultParam{0.0};
+};
+#endif
---
+++ Coccolith/src/voxelMaterial2D.cpp
@@ -0,0 +1,103 @@
+#include "voxelMaterial2D.hpp"
+#include <stdexcept>
+#include <cassert>
+
+using namespace std;
+
+arma::Mat<unsigned char> VoxelMaterial2D::voxels;
+
+void VoxelMaterial2D::extractInfoFromFilename( const string &fname, FileInfo &info )
+{
+  unsigned int values[3];
+  unsigned int pos = 0;
+  for ( unsigned int i=0;i<3;i++ )
+  {
+    pos = fname.find("_", pos+1);
+    if ( pos == string::npos )
+    {
+      throw (runtime_error("Wrong filename format. Expected <somefilename>_resolution_Nx_Ny.raw"));
+    }
+    stringstream ss;
+    unsigned int length = 0;
+
+    if ( !isdigit(fname[pos+1]) )
+    {
+      throw( runtime_error("The first number after the underscore is not a digit!") );
+    }
+    for (unsigned int j=pos+1;j<fname.length();j++ )
+    {
+      if ( !isdigit(fname[j]) ) break;
+      length++;
+    }
+
+    ss << fname.substr( pos+1, length );
+    ss >> values[i];
+  }
+
+  info.resolutionNM = values[0]/10.0;
+  info.Nx = values[1];
+  info.Ny = values[2];
+}
+
+void VoxelMaterial2D::loadRaw( const string &fname )
+{
+  voxels.load( fname.c_str(), arma::raw_binary );
+  FileInfo info;
+  extractInfoFromFilename( fname, info );
+
+  if ( info.Nx*info.Ny != voxels.n_elem )
+  {
+    throw( runtime_error("The dimensions from the filename, does not match the number of elements read!"));
+  }
+  voxels.reshape( info.Nx, info.Ny );
+  applyThreshold();
+}
+
+void VoxelMaterial2D::loadRaw( const char* fname )
+{
+  string fnameCppString(fname);
+  loadRaw(fnameCppString);
+}
+
+void VoxelMaterial2D::applyThreshold()
+{
+  for ( unsigned int i=0;i<voxels.n_cols;i++ )
+  {
+    for ( unsigned int j=0;j<voxels.n_rows;j++ )
+    {
+      voxels(j,i) = voxels(j,i) > threshold ? 1:0;
+    }
+  }
+}
+
+bool VoxelMaterial2D::isOutsideComputationalDomain( const meep::vec &r ) const
+{
+  return (r.x() > domain.xmax ) || (r.x() < domain.xmin ) || ( r.y() < domain.ymin ) || ( r.y() > domain.ymax );
+}
+
+unsigned char VoxelMaterial2D::get( const meep::vec &r ) const
+{
+  assert( !isOutsideComputationalDomain(r) );
+  unsigned int x = Nx()*( r.x()-domain.xmin)/(domain.xmax-domain.xmin);
+  unsigned int y = Ny()*( r.y()-domain.ymin)/(domain.ymax-domain.ymin);
+  return get(x,y);
+}
+
+bool VoxelMaterial2D::isInside( const meep::vec& r ) const
+{
+  if ( isOutsideComputationalDomain(r) ) return false;
+  return get(r) == 1;
+}
+
+// ===================== DISPERSIVE 2D VOXEL MATERIAL ==========================
+double Voxel2DSusceptibility::f( const meep::vec &r )
+{
+  if ( isInside(r) ) return sigma;
+  return defaultParam;
+}
+
+void Voxel2DSusceptibility::sigma_row( meep::component c, double sigrow[3], const meep::vec &r )
+{
+  sigrow[0] = sigrow[1] = sigrow[2] = 0.0;
+  sigrow[meep::component_index(c)] = f(r);
+}
