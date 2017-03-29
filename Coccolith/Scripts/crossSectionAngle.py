5269a7c2dc2641a1de38e9bb4ea8d6f97a7720d6
David Kleiven <davidkleiven446@gmail.com>
2017-03-29 08:54:51 +0200

    Added comparison for the asymmetry factor

--- Coccolith/Scripts/crossSectionAngle.py
+++ Coccolith/Scripts/crossSectionAngle.py
@@ -32,6 +32,8 @@ class ScatteringPlots:
             raise Exception("No dataset named geometrySourceVolume!")
         elif ( not "spectrumFreqs" in keys ):
             raise Exception("No dataset named spectrumFreqs!")
+        elif ( not "Asymmetry" in keys ):
+            raise( Exception("No dataset named Asymmetry!"))
 
     def crossSection( self ):
         fig = plt.figure()
@@ -52,7 +54,14 @@ class ScatteringPlots:
                 dx = geosize[3]-geosize[0]
                 dy = geosize[4]-geosize[1]
                 dz = geosize[5]-geosize[2]
-                crossSection = dy*dz
+                if ( entry["propagationDirection"] == "x" ):
+                    crossSection = dy*dz
+                elif ( entry["propagationDirection"] == "y" ):
+                    crossSection = dx*dz
+                elif ( entry["propagationDirection"] == "z" ):
+                    crossSection = dx*dy
+                else:
+                    raise Exception("Propagation direction has to x, y or z!")
                 scatCross = np.abs( (scatFlux-refFlux)*vxsize**2/( influx ) )
                 wavelength = vxsize/f
                 ax.plot( wavelength, scatCross, color=entry["color"], label="\$\SI{%d}{\degree}\$"%(entry["angle"]))
@@ -65,12 +74,34 @@ class ScatteringPlots:
         ax.legend(loc="upper center", frameon=False, labelspacing=0.05)
         return fig, ax
 
+    def asymmetry( self ):
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+        for entry in self.input["datasets"]:
+            with h5.File(entry["file"], 'r') as hf:
+                asym = np.array( hf.get("Asymmetry") )
+                freqs = np.array( hf.get("spectrumFreqs") )
+                f = np.linspace( freqs[0], freqs[0]+freqs[1]*freqs[2], freqs[2] )
+                vxsize = np.array( hf.get("vxSizeNM") )[0]
+                wavelength = vxsize/f
+                ax.plot( wavelength, asym, color=entry["color"], label="\$\SI{%d}{\degree}\$"%(entry["angle"]))
+        ax.set_xlabel("Wavelength (nm)")
+        ax.set_ylabel("Asymmetry factor, \$g\$")
+        ax.spines["right"].set_visible(False)
+        ax.spines["top"].set_visible(False)
+        ax.yaxis.set_ticks_position("left")
+        ax.xaxis.set_ticks_position("bottom")
+        ax.legend(loc="best", frameon=False, labelspacing=0.05)
+        return fig, ax
+
+
 def main( argv ):
     if ( len(argv) != 1 ):
         print ("Usage: python crossSectionAngle.py inputfile.json")
     try:
         plots = ScatteringPlots(argv[0])
         plots.crossSection()
+        plots.asymmetry()
         plt.show()
     except Exception as exc:
         print (str(exc))
