2e5e3d62e373f367cd6f51db3ca9476af4f8190e
David Kleiven <davidkleiven446@gmail.com>
2017-04-10 10:47:26 +0200

    Added function for merging the resulting hdf5 files

--- Coccolith/Scripts/runMuellerMatrix.py
+++ Coccolith/Scripts/runMuellerMatrix.py
@@ -4,6 +4,7 @@ import json
 import numpy as np
 import h5py as h5
 import multiprocessing as mp
+import time
 
 tempDir = "data/muellerCalculation"
 templateInputFile = "InputFiles/visibleLightCaCO3ne.json"
@@ -29,9 +30,38 @@ def launch( muellerSim ):
     subprocess.call(["mpiexec", "-n", "12", "./coccolith.out", ofname])
 
 def mergeHDF5s():
-    for 
+    tstamp = time.strftime("%Y%m%d_%H%M")
+    hfile = h5.File( "data/muellerMatrix_%s.h5"%(tstamp) )
+    isFirst = True
+    counter = 0
+    for fname in os.listdir(tempDir):
+        if (fname.find(".h5") == -1 ):
+            continue
 
-def main():
+        with h5.File(tempDir+"/"+fname) as hf:
+            if ( isFirst ):
+                hfile.create_dataset("eps", data=np.array(hf.get("eps")))
+                isFirst = False
+
+            group = hfile.create_group("Run%d"%(counter))
+            counter += 1
+            group.create_dataset("freq", data=np.array( hf.get("freq") ) )
+
+            # Collect everything that contains stokes or Stokes
+            for key in hf.keys():
+                if (( "stokes" in key ) or ("Stokes" in key) or ("stoke" in key) or ("Stoke" in key)):
+                    group.create_dataset(key,data=np.array(hf.get(key)))
+        # Clean up
+        subprocess.call(["rm", tempDir+"/"+fname])
+    hfile.close()
+
+def main( argv ):
+    global templateInputFile
+    if  ( len(argv) != 1 ):
+        print ("Usage: python InputFileTemplate.json")
+        return
+    templateInputFile = argv[0]
+    
     # Create a temporary directory
     subprocess.call(["mkdir", "-p", tempDir])
 
@@ -45,3 +75,4 @@ def main():
 
     workers = mp.Pool(2)
     workers.map( launch, stokesSimulations )
+    mergeHDF5s()
